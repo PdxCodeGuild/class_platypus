1e438daf98a84a8e19aca91a5da404e83508ed00
Mitchell Estes <mestes1530@gmail.com>
2018-08-13 16:25:27 -0700

    Added more face options.

--- Code/Mitchell/lab05.py
+++ Code/Mitchell/lab05.py
@@ -1,5 +1,5 @@
 import random
-eyes = [':', ';', '8']
-nose = ['^', 'c', '>', '-']
-mouth = [')', '|', '(', 'P', 'O', '/']
+eyes = [':', ';', '8', '=']
+nose = ['^', 'c', '>', '-', '*', 'v']
+mouth = [')', '|', '(', 'P', 'O', '/', ']']
 print('Here is a randomly generated emoticon:  ' + random.choice(eyes) + ' ' + random.choice(nose) + ' ' + random.choice(mouth))
