5c012c77260856b2daf97bfc0f6ed2d01ea0b7a5
induts <induts@gmail.com>
2018-08-13 13:47:42 -0700

    stickman

---
+++ Code/Indu/stickmancirclehead.py
@@ -0,0 +1,17 @@
+from turtle import *
+headsize = 50
+angle = 90
+torso= 120
+armlength= 100
+leg=350
+circle(headsize)
+right(90)
+forward(torso)
+right(30)
+forward(leg)
+penup()
+backward(leg)
+pendown()
+left(60)
+forward(leg)
+penup()
