b3a58b90def36c8fda8e19d8343874eba3e4ec22
ColtonJP <colton.peterson06@gmail.com>
2018-08-13 16:27:58 -0700

    lab_05 simplified.

--- Code/Colton/Lab_05.py
+++ Code/Colton/Lab_05.py
@@ -5,6 +5,5 @@ mouth_list = (')','(', 'p', 'D' )
 i = 0
 while i < 5:
     i = i + 1
-    out_string = ''
-    out_string = out_string + random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
+    out_string = random.choice(eye_list) + random.choice(nose_list) + random.choice(mouth_list)
     print(out_string)
