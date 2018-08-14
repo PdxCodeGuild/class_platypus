98c5e9ebfe5354258cf6da28b149d2076c883969
Aceome <angelina.bowmer@gmail.com>
2018-08-14 15:41:50 -0700

    lab 6-updated import and deleted comments

--- Code/Angie/lab06-passgen.py
+++ Code/Angie/lab06-passgen.py
@@ -1,17 +1,9 @@
-import string
-import random
+import string, random
+
 
 x = ''
-for i in range(10):
+for i in range(18):
     c = random.choice(string.printable + string.digits)
-    print(x)
     x = x + c
 print(x)
 
-# 'string builder pattern'
-
-#passwords = []
-#for x in passwords:
-#    x += random.choice(string.printable + string.digits)
- #   x += str(1)
-#print(x)
