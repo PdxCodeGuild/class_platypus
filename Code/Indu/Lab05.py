675b54f9dbe243c6199f6bee8aa893113a857280
induts <induts@gmail.com>
2018-08-14 09:43:52 -0700

    finished

--- Code/Indu/Lab05.py
+++ Code/Indu/Lab05.py
@@ -1,9 +1,9 @@
 # Random emoticon generator
 import random
 eyes = [':','D','o','$','%',':']
-nose = ['|','','o']
+nose = ['|','i','o']
 mouth = ['(',')','O','D']
 out_string = ''
-for i in [0,1,2,3,4,5,6]:
+for i in [0,1,2,3]:
   out_string = out_string + random.choice(eyes)+random.choice(nose)+random.choice(mouth)
 print(out_string)
