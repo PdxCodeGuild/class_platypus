fed5bd358fb83b5dc8234c6a220d84cc62aaec67
induts <induts@gmail.com>
2018-08-14 14:43:34 -0700

    Version1

--- Code/Indu/Lab08.py
+++ Code/Indu/Lab08.py
@@ -1,6 +1,7 @@
 # Simple calculator
 #created by Indu Thekkemeppilly Sivakumar in 08/14/2018
 operation_list = ['addition''subtraction''multiplication''division']
+print("The available operations are addition,subraction,multiplication,division")
 operation = input("What is the operation you would like to perform? \n")
 firstnum = float(input("What is the first number?\n"))
 secondtnum = float(input("What is the second number?\n"))
