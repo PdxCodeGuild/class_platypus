cd858903b7dbf3e63ed494df748cb2025d1a43e8
kagomesakura <4mygithub>
2018-08-14 15:38:51 -0700

    simple calculator added while loop

--- Code/Kagome/lab11calculator.py
+++ Code/Kagome/lab11calculator.py
@@ -1,16 +1,25 @@
 
-ans = input('What operation would you like to perform? select(+) for add, \nselect(-) for subtract, select(*) formultiple, or (/)for divide(/)')
-num1 = float(input('What is the first number? '))
-num2 = float(input('What is the second number? '))
+while True:
+    ans = input('What operation would you like to perform? select(+) for add, \nselect(-) for subtract, select(*) formultiple, or (/)for divide(/)')
+    num1 = float(input('What is the first number? '))
+    num2 = float(input('What is the second number? '))
 
-if ans == '+':
-    print(num1 + num2)
-if ans == '-':
-    print(num1 - num2)
-if ans == '*':
-    print(num1 * num2)
-if ans == '/':
-    print(num1 / num2)
+    if ans == '+':
+        print(num1 + num2)
+    if ans == '-':
+        print(num1 - num2)
+    if ans == '*':
+        print(num1 * num2)
+    if ans == '/':
+        print(num1 / num2)
+
+    continuecalc = input('Would you like to continue using the calculator? yes(y) or done(d)')
+    if continuecalc == 'y':
+        print(ans)
+
+    if continuecalc == 'd':
+        break
+
 
 
 
