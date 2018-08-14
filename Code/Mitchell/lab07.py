4b7e7576eba1d65bf88a4e71a0d9c60e1e914673
Mitchell Estes <mestes1530@gmail.com>
2018-08-14 12:31:32 -0700

    Cleaned up code.

--- Code/Mitchell/lab07.py
+++ Code/Mitchell/lab07.py
@@ -9,24 +9,20 @@ while playAgain == 'yes':
     #Tie outcome
     if humanChoice == computerChoice:
         print('I also chose ' + humanChoice + ', so it is a TIE!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
     #Human wins outcomes
-    if humanChoice == 'paper' and computerChoice == 'rock':
+    elif humanChoice == 'paper' and computerChoice == 'rock':
         print('Dang! I chose ' + computerChoice + ', so you WIN because your' + humanChoice + ' beats my ' + computerChoice + '!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
-    if humanChoice == 'rock' and computerChoice == 'scissors':
+    elif humanChoice == 'rock' and computerChoice == 'scissors':
         print('Dang! I chose ' + computerChoice + ', so you WIN because your' + humanChoice + ' beats my ' + computerChoice + '!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
-    if humanChoice == 'scissors' and computerChoice == 'paper':
+    elif humanChoice == 'scissors' and computerChoice == 'paper':
         print('Dang! I chose ' + computerChoice + ', so you WIN because your' + humanChoice + ' beats my ' + computerChoice + '!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
     #Computer wins outcomes
-    if computerChoice == 'paper' and humanChoice == 'rock':
+    elif computerChoice == 'paper' and humanChoice == 'rock':
         print('Sweet! I chose ' + computerChoice + ', so you LOSE because my ' + computerChoice + ' beats your ' + humanChoice + '!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
-    if computerChoice == 'rock' and humanChoice == 'scissors':
+    elif computerChoice == 'rock' and humanChoice == 'scissors':
         print('Sweet! I chose ' + computerChoice + ', so you LOSE because my ' + computerChoice + ' beats your ' + humanChoice + '!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
-    if computerChoice == 'scissors' and humanChoice == 'paper':
+    elif computerChoice == 'scissors' and humanChoice == 'paper':
         print('Sweet! I chose ' + computerChoice + ', so you LOSE because my ' + computerChoice + ' beats your ' + humanChoice + '!')
-        playAgain = input('Do you want to play again? (Type "yes" or "no")')
+    else:
+        print('ERROR: Please type ether "rock", "paper" or "scissors')
+    playAgain = input('Do you want to play again? (Type "yes" or "no")')
