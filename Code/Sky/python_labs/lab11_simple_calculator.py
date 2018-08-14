4fb0704d67af4dd674215bba065b2179ac2491f2
Skyler <me@skylerparker.com>
2018-08-14 14:30:44 -0700

    Changed formatting of answer

--- Code/Sky/python_labs/lab11_simple_calculator.py
+++ Code/Sky/python_labs/lab11_simple_calculator.py
@@ -13,9 +13,9 @@ while True:
     menu_answer = int(input('\nMenu:\n\n1 - Enter a new equation\n2 - Close Program'))
 
     if menu_answer == 1:
-        expression_to_evaluate = input('Please enter an expression that you would like to have evaluated: ')
+        expression_to_evaluate = input('\nPlease enter an expression that you would like to have evaluated: ')
         answer = eval(expression_to_evaluate)
-        print('Your answer is ' + str(answer))
+        print('\nYour answer is ' + str(answer))
     else:
         break
 
