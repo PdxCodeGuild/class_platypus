4e0c5dfdb24aa18d21a9134ac075dfaa7f06493d
kagomesakura <4mygithub>
2018-08-14 15:24:42 -0700

    simple calculator

---
+++ Code/Kagome/lab11calculator.py
@@ -0,0 +1,30 @@
+
+ans = input('What operation would you like to perform? select(+) for add, \nselect(-) for subtract, select(*) formultiple, or (/)for divide(/)')
+num1 = float(input('What is the first number? '))
+num2 = float(input('What is the second number? '))
+
+if ans == '+':
+    print(num1 + num2)
+if ans == '-':
+    print(num1 - num2)
+if ans == '*':
+    print(num1 * num2)
+if ans == '/':
+    print(num1 / num2)
+
+
+
+
+
+
+# def add(x, y):
+#    return x + y
+#
+# def subtract(x, y):
+#    return x - y
+#
+# def multiply(x, y):
+#    return x * y
+#
+# def divide(x, y):
+#    return x / y
