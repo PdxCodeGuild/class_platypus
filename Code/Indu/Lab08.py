ddf22af80c8066f905656a8ea1d53ff94fc07a05
induts <induts@gmail.com>
2018-08-14 14:41:03 -0700

    Version1

---
+++ Code/Indu/Lab08.py
@@ -0,0 +1,15 @@
+# Simple calculator
+#created by Indu Thekkemeppilly Sivakumar in 08/14/2018
+operation_list = ['addition''subtraction''multiplication''division']
+operation = input("What is the operation you would like to perform? \n")
+firstnum = float(input("What is the first number?\n"))
+secondtnum = float(input("What is the second number?\n"))
+#print(firstnum+secondtnum)
+if operation == 'addition':
+    print(f"{firstnum}+{secondtnum} is {(firstnum+secondtnum)}")
+elif operation == 'subtraction':
+    print(f"{firstnum}-{secondtnum} is {(firstnum-secondtnum)}")
+elif operation == 'multiplication':
+    print(f"{firstnum} x {secondtnum} is {(firstnum*secondtnum)}")
+elif operation == 'division':
+    print(f"{firstnum}/{secondtnum} is {(firstnum/secondtnum)}")
