d226911d92415d366803b9dd1c00eddd7276899a
induts <induts@gmail.com>
2018-08-15 12:01:53 -0700

    Ft to meter conversion

---
+++ Code/Indu/Lab12version4.py
@@ -0,0 +1,8 @@
+#Guess the number:
+# Objective: Tell the user whether their current guess is closer than their last. This can be done by maintaining a variable
+# containing the last guess outside the loop, then comparing the last guess to the current guess, and check if it's closer.
+#  Hint: you're interested in comparing the two absolute differences: abs(current_guess-target) and abs(last_guess-target)
+
+#created by Indu Thekkemeppilly Sivakumar on 08-15-2018
+import random
+comp_num = random.randint(0,10)
