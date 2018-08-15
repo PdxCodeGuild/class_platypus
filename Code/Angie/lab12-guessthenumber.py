41bea8bd179980290b30865f046c4c257954ad5c
Aceome <angelina.bowmer@gmail.com>
2018-08-15 09:59:27 -0700

    lab 12-update added limit of 10 guesses

--- Code/Angie/lab12-guessthenumber.py
+++ Code/Angie/lab12-guessthenumber.py
@@ -1,19 +1,23 @@
 import random
 
 x = random.randint(1, 10)
-print(x)
 counter = 0
 
 
 while True:
 
-    user = int(input('Guess the number between 1 and 10\n'))
+    user = int(input('Guess the number between 1 and 10, you get 10 tries.\n'))
     counter += 1
     if x == user:
-        print(f'Winner Winner Chicken Dinner {counter}')
+        print(f'Winner Winner Chicken Dinner! You guessed {counter} times')
         break
+
+    if counter == 10:
+        print('you guessed 10 times! Sorry you lose.')
+        break
+
     else:
-        print(f'Sorry try again {counter}')
+        print(f'Sorry try again! You guessed {counter} times')
 
     perform = input('Do you want to guess again?').lower()
     if perform == 'no':
