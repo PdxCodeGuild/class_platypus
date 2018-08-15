29fb3b1c86cf56165b74256aa42cda16521100cc
Mitchell Estes <mestes1530@gmail.com>
2018-08-15 13:55:25 -0700

    Lets user convert the output units to be any one of the input units allowed.

--- Code/Mitchell/lab09.py
+++ Code/Mitchell/lab09.py
@@ -1,5 +1,10 @@
+#Prompts to recieve input
 enteredDistance = input('What is the distance?')
-enteredUnit = input('What are the units?').lower()
+enteredUnit = input('What are the input units?').lower()
+finalUnit = input('What are the output units?').lower()
 conversion = {'ft': 0.3048, 'mi': 160934, 'km': 1000, 'yd': 0.9144, 'in': 0.0254}
-finalDistance = format(float(enteredDistance) * conversion[enteredUnit], '.4g')
-print(f'{enteredDistance} {enteredUnit} is {finalDistance} m')
+#Converts the input to meters
+meterDistance = float(enteredDistance) * conversion[enteredUnit]
+#Converts from meters to output units
+finalDistance = format(meterDistance / float(conversion[finalUnit]), '.4g')
+print(f'{enteredDistance} {enteredUnit} is {finalDistance} {finalUnit}')
