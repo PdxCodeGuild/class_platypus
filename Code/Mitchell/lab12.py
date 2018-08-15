527223311a0fcd7fadcd835780a52d517f6f94b9
Mitchell Estes <mestes1530@gmail.com>
2018-08-15 10:31:30 -0700

    Added another mode where the computer tries to guess user's number.

--- Code/Mitchell/lab12.py
+++ Code/Mitchell/lab12.py
@@ -1,13 +1,42 @@
 import random
-theNum = str(random.randint(1,10))
-legalGuesses = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
-guessing = 'yes'
-while guessing == 'yes':
-    guessed = input('I am think of a number between 1 and 10, what do you think it is?')
-    if int(guessed) == int(theNum):
-        print('You got it! My number was ' + theNum + '!')
-        guessing = 'no'
-    elif int(guessed) > int(theNum):
-        guessing = input('Nope, my number is lower than that. Guess again? (Type "yes" or "no")')
-    elif int(guessed) < int(theNum):
-        guessing = input('Nope, my number is higher than that. Guess again? (Type "yes" or "no")')
+#Starting prompt to choose game mode
+start = input('To play the Number Guessing Game type ether "think" if you want to think a number '
+              'and I try and guess \nit or type "guess" if you want me to think of a number and you guess it:').lower()
+#Computer thinks of a number and user guesses it
+if start == 'guess':
+    theNum = str(random.randint(1,10))
+    guessing = 'yes'
+    while guessing == 'yes':
+        guessed = input('I am think of a number between 1 and 10, what do you think it is?')
+        #User guessed computer's number
+        if int(guessed) == int(theNum):
+            print('You got it! My number was ' + theNum + '!')
+            guessing = 'no'
+        #User's guess was too high
+        elif int(guessed) > int(theNum):
+            guessing = input('Nope, my number is lower than that. Guess again? (Type "yes" or "no")').lower()
+        #User's guess was too low
+        elif int(guessed) < int(theNum):
+            guessing = input('Nope, my number is higher than that. Guess again? (Type "yes" or "no")').lower()
+#User thinks of a number and computer guesses it
+elif start == 'think':
+    input('Think of a number between 1 and 10, then press the Enter key when you have it.')
+    thinking = 'yes'
+    x = 1
+    y = 10
+    while thinking == 'yes':
+        randomNum = str(random.randint(x, y))
+        guess = input('Is it ' + randomNum + '? (Type ether "yes", "higher" or "lower")').lower()
+        #Computer guest the user's number
+        if guess == 'yes':
+            print('Sweet I knew I could guess it!')
+            thinking = 'no'
+        #Computer's guess was too low
+        elif guess == 'higher':
+            x = int(randomNum) + 1
+        # Computer's guess was too high
+        elif guess == 'lower':
+            y = int(randomNum) - 1
+#Invalid game mode entered
+else:
+    print('ERROR: Invalid game mode as nether "guess" or "think" were entered.')
