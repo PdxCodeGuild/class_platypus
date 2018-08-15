1e6b425029827be74335fc7ce8f1fbf55fd2b6b0
Aceome <angelina.bowmer@gmail.com>
2018-08-15 15:20:47 -0700

    lab09V2-update added yards and inches

--- Code/Angie/lab09-unitconverterV2.py
+++ Code/Angie/lab09-unitconverterV2.py
@@ -1,5 +1,5 @@
 
-units = {'ft': 0.3048, 'mi': 1609.34, 'km': 1000, 'm': 1}
+units = {'ft': 0.3048, 'mi': 1609.34, 'km': 1000, 'm': 1, 'yd': 0.9144, 'in': 0.0254}
 
 #y = 0.3048
 
@@ -7,7 +7,7 @@ while True:
     user = float(input('what is the distance?\n'))
     input_units = ''
     while input_units not in units:
-        input_units = input(f'What are the units? ({", ".join(units.keys())}) ')
+        input_units = input(f'What are the units? ({", ".join(units.keys())})\n').lower()
     # if input_units == 'ft':
     #     value = user * 0.3048
     # elif input_units == 'mi':
