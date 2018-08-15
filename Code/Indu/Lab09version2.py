55fc7f5b9a27b586135358a21b30e65b2a6a52bf
induts <induts@gmail.com>
2018-08-15 12:22:52 -0700

    added documentation

--- Code/Indu/Lab09version2.py
+++ Code/Indu/Lab09version2.py
@@ -1,3 +1,5 @@
+#convert units to meter
+#created by Indu Thekkemeppilly Sivakumar on 08/15/2018
 inunit= input("What is the unit of your input? feet,meters,Kilometer,mile\n")
 distance= float(input("What is the distace?"))
 convert2meter = {'feet':0.3048, 'meters':1, 'kilometer':1000, 'mile':1609.34}
