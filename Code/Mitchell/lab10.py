e50b257bdf0a1a405bcbd21cbee5e4c716f9672e
Mitchell Estes <mestes1530@gmail.com>
2018-08-16 10:15:36 -0700

    Lets user enter values for list that is averaged.

--- Code/Mitchell/lab10.py
+++ Code/Mitchell/lab10.py
@@ -1,6 +1,12 @@
-nums = [5, 0, 8, 3, 4, 1, 6]
+nums = []
 sum = 0
+inputValue = ''
+#Recieves input from user to fill loop
+while inputValue != 'done':
+    inputValue = input('Enter a number, or "done": ').lower()
+    if inputValue != 'done':
+        nums.append(inputValue)
 #Loop over the indices
 for i in range(len(nums)):
-    sum = sum + int(nums[i])
-print(sum / len(nums))
+        sum = sum + int(nums[i])
+print('Average: ' + str(sum / len(nums)))
