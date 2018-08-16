17d04b64fe9a59176efe65919c5eeac0ca2a22e0
Skyler <me@skylerparker.com>
2018-08-16 11:11:02 -0700

    ...

--- Code/Sky/python_labs/lab10_average number.py
+++ Code/Sky/python_labs/lab10_average number.py
@@ -4,6 +4,7 @@
 
 # Imports
 import random
+import math
 # Define global variables and lists
 
 # Define Functions
@@ -23,6 +24,39 @@ def median(nums):
     total = float(0)
     nums.sort()
     print(nums)
+    if len(nums) % 2 == 1:
+        median_loc = math.ceil(len(nums)/2 - 1)
+        print('The median numbers are: ' + nums[median_loc])
+    else:
+        medians = []
+        median_loc = int(len(nums)/2)
+        medians.append(nums[median_loc - 1])
+        medians.append(nums[median_loc])
+        print('The median numbers are: ' + str(medians[0]) + ' and ' + str(medians[1]))
+
+
+def mode(nums):
+    number_count = {}
+    for number in nums:
+        if number in number_count:
+            print(number_count[number])
+            number_count[number] = number_count[number] + 1
+        else:
+            number_count[number] = 1
+
+    mode_numbers = {}
+    median_amount = 0
+    for number in number_count:
+        if number_count[number] > median_amount:
+            median_amount = number_count[number]
+
+    print median amount
+
+
+
+    print('----')
+    print(number_count)
+
 
 
 # Program Begins
@@ -41,9 +75,9 @@ while True:
             print('\n\nYou entered no numbers, would you like to try again?')
         else:
             print('\n')
-            print(numbers)
             # averages(numbers)
-            median(numbers)
+            # median(numbers)
+            mode(numbers)
 
     else:
         break
