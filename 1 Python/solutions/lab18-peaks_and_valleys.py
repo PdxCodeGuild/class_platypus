53328ba0d6fe69886ccef9f6365def25ba68ca2f
flux2341 <flux2341@gmail.com>
2018-08-16 15:22:15 -0700

    added peaks and valleys solution

--- 1 Python/solutions/lab18-peaks_and_valleys.py
+++ 1 Python/solutions/lab18-peaks_and_valleys.py
@@ -13,11 +13,11 @@ a 'valley' is a number which is less that the number on the left and right
                   X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X
                X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X
             X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X  X
->>> data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
->>> peaks(data)
-[6, 14]
->>> valleys(data)
-[9, 17]
+# >>> data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+# >>> peaks(data)
+# [6, 14]
+# >>> valleys(data)
+# [9, 17]
 """
 
 
@@ -45,19 +45,13 @@ def valleys(data):
     return valley_indices
 
 
-def main():
-    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-    print(peaks(data))
-    print(valleys(data))
-
-main()
 
 '''
 Here's an alternative version, using a regular for-loop with continue to over the first and last values.
 
 def peaks(data):
     peak_indices = []
-    for i in range(0, len(data)): #loop through the list, avoiding the end-points
+    for i in range(len(data)): #loop through the list, avoiding the end-points
         if i == 0 or i == len(data)-1:
             continue
         left = data[i-1]
@@ -67,5 +61,93 @@ def peaks(data):
             peak_indices.append(i)
     return peak_indices
 '''
+
+
+def draw(data):
+    largest = max(data)
+    for i in range(largest, 0, -1):
+        row = ''
+        for j in range(len(data)):
+            if i <= data[j]:
+                row += 'X'
+            else:
+                row += ' '
+        print(row)
+
+# ;_;
+# def let_it_rain_matthew_failure(data):
+#     water = 0
+#     for j in range(max(data), 0, -1):
+#         row = ''
+#         starting_index = 0
+#         while starting_index < len(data):
+#             while data[starting_index] < j and starting_index < len(data):
+#                 starting_index += 1
+#                 print(' ', end='')
+#             print('X')
+#
+#         # while starting_index < len(data):
+#         #     while data[starting_index] >= j:
+#         #         starting_index += 1
+#         #         if starting_index >= len(data):
+#         #             break
+#         #     ending_index = starting_index
+#         #     while data[ending_index] < j:
+#         #         ending_index += 1
+#         #         if ending_index >= len(data):
+#         #             break
+#         #     water += ending_index - starting_index
+#
+#     print(water)
+
+
+def draw_filled_maggie(data):  # uses a toggle to switch from space to fill
+    graph = ''
+    fill = False
+    water = 0
+    for line in range(max(data)):  # start with line 1, iterate through each line
+        for i in range(len(data)):  # iterate through each space (x value) in the line
+            if data[i] >= (max(data) - line):  # the first line should be the max, second line = max -1
+                graph += ' x '
+                fill = True
+            else:
+                if fill:
+                    graph += ' 0 '
+                    water += 1
+                else:
+                    graph += '   '
+        if line != max(data) - 1:
+            graph += '\n'  # skip a line at the end of each row, except for last line
+        fill = False
+    print(graph)
+    print(data)
+    print('amount of water collected: ', water, 'units')
+
+
+
+
+
+def main():
+    data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9, 8, 7, 6, 5, 4]
+    #print(peaks(data))
+    #print(valleys(data))
+    draw(data)
+    print()
+    print()
+    print()
+    draw_filled(data)
+
+main()
+
+
+
+
+
+
+
+
+
+
+
 
 
