ab2bda59407fb3c10df12a308c12d80c228af2d2
Skyler <me@skylerparker.com>
2018-08-16 16:03:52 -0700

    ...

--- Code/Sky/python_labs/lab15_number_to_phras.py
+++ Code/Sky/python_labs/lab15_number_to_phras.py
@@ -14,19 +14,31 @@ while True:
 
     if menu_answer == 1:
 
-        phrases = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'twenty',
-                   'thirty',
-                   'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'one hundred', 'two hundred',
-                   'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred',
-                   'nine hundred']
+        ones_place = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
         teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+        tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
         user_number = int(input('What number would like converted?'))
 
         if 0 <= user_number <= 10:
-            print('\n\nThe number you entered is spelled ' + phrases[user_number])
+            print('\n\nThe number you entered is spelled ' + ones_place[user_number])
+
         elif 11 <= user_number <= 19:
             print('\n\nThe number you entered is spelled ' + teens[user_number - 11])
+
+        elif 20 <= user_number <= 99:
+            tens = user_number//10
+            ones = user_number%10
+            print(tens_place[tens - 1] + '-' + ones_place[ones])
+        elif 100 <= user_number <= 999:
+            hundreds = user_number//100
+            tens = (user_number % 100)//10
+            ones = user_number % 10
+
+            print('\n\nThe number you entered is spelled ' + phrases)
+
+        else:
+            print('The number must be 0 - 999; try again')
 
 
 
