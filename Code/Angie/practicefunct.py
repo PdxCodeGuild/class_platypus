4149635528c0dff81925fb847486617ab57dd2bf
Aceome <angelina.bowmer@gmail.com>
2018-08-16 17:38:45 -0700

    updates

--- Code/Angie/practicefunct.py
+++ Code/Angie/practicefunct.py
@@ -1,5 +1,12 @@
-def is_even(a):
-    if type(a) is int:
-        return a % 2 == 0
-    else:
-        return -1
+# def is_even(a):
+#     if type(a) is int:
+#         return a % 2 == 0
+#     else:
+#         return -1
+
+
+def double(s):
+    out_string = ''
+    for i in s:
+        out_string = out_string + i * 2
+    return out_string
