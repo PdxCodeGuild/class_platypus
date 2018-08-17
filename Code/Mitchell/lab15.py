6f99a9412c9101024c7e07b1bf522367c1a2c33f
Mitchell Estes <mestes1530@gmail.com>
2018-08-17 10:10:17 -0700

    Can now convert 3 digit numbers to words.

--- Code/Mitchell/lab15.py
+++ Code/Mitchell/lab15.py
@@ -1,9 +1,20 @@
 # Dictionaries with words for each number
 tensDigits = {2: 'twenty', 3: 'thir', 4: 'four', 5: 'fif', 6: 'six', 7: 'seven', 8: 'eigh', 9: 'nine'}
 onesDigits = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
-x = 90
-tens_digit = x//10
-ones_digit = x%10
+num = int(input('Enter a number to have it spelt out: '))
+hundreds_digit = num//100
+#tens_digit defined bellow
+ones_digit = num%10
+#Cases for three digit numbers
+if hundreds_digit > 0:
+    print(hundreds_digit)
+    print(onesDigits[hundreds_digit] + ' hundred ', end='')
+    num = num - (100 * hundreds_digit)
+#Define tens_digit after removing hundreds digit
+tens_digit = num//10
+#Cases for multiples of one hundred
+if tens_digit == 0 and ones_digit == 0:
+    exit()
 #Cases for single digit
 if tens_digit == 0 or tens_digit == '':
     print(onesDigits[ones_digit])
