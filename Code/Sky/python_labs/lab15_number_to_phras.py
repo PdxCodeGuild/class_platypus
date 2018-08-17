03252b1a7a6736457516a0eb3e894dec69ca11d8
Skyler <me@skylerparker.com>
2018-08-17 13:35:02 -0700

    ...

--- Code/Sky/python_labs/lab15_number_to_phras.py
+++ Code/Sky/python_labs/lab15_number_to_phras.py
@@ -7,73 +7,62 @@
 # Define global variables and lists
 
 # Define Functions
+
+
 def roman_numeral_conversion(input_number):
     roman_numerals = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
     converted_number = ''
 
-    if len(input_number) == 3:
-        temp_input_number = input_number//100
-        index_add = 2
+    if len(str(input_number)) == 4:
+        temp_input_number = input_number//1000
+        next_number = input_number%1000
+        index_add = 6
 
-    elif len(input_number) == 2:
+    if len(str(input_number)) == 3:
+        temp_input_number = input_number//100
+        next_number = input_number%100
+        index_add = 4
+
+    elif len(str(input_number)) == 2:
         temp_input_number = input_number//10
-        index_add = 1
+        next_number = input_number % 10
+        index_add = 2
     else:
         temp_input_number = input_number
         index_add = 0
 
+    if temp_input_number == 9:
+        converted_number += roman_numerals[0 + index_add] + roman_numerals[2 + index_add]
 
-user_number = int(input('Please enter a number between 1 and 1000 to be converted into Roman Numerals'))
-        # print('The Roman Numeral equivalent is ', end='')
-        # if 1000 <= user_number <= 3000:
-        #     converted_number += roman_numerals[6]
-        #
-        # if 900 <= user_number <= 999:
-        #     converted_number += roman_numerals[4] + roman_numerals[6]
-        #
-        # if 800 <= user_number <= 899:
-        #     converted_number += roman_numerals[5] + roman_numerals[4]*3
-        #
-        # if 700 <= user_number <= 799:
-        #     converted_number += roman_numerals[5] + roman_numerals[4]*2
-        #
-        # if 600 <= user_number <= 699:
-        #     converted_number += roman_numerals[5] + roman_numerals[4]
-        #
-        # if 500 <= user_number <= 599:
-        #     converted_number += roman_numerals[5]
-        #
-        # if 400 <= user_number <= 499:
-        #     converted_number += roman_numerals[4] + roman_numerals[5]
-        #
-        # if 300 <= user_number <= 399:
-        #     converted_number += roman_numerals[4]*3
-        #
-        # if 200 <= user_number <= 299:
-        #     converted_number += roman_numerals[4]*2
-        #
-        # if 100 <= user_number <= 199:
-        #     converted_number += roman_numerals[4]
-        #
-        # if 90 <= user_number <= 99 or 90 <= user_number % 100 <= 99:
-        #     converted_number += roman_numerals[2] + roman_numerals[4]
-        #
-        # if 80 <= user_number <= 89 or 80 <= user_number % 100 <= 89:
-        #     converted_number += roman_numerals[2] * 3
-        #
-        # if 70 <= user_number <= 79 or 70 <= user_number % 100 <= 79:
-        #     converted_number += roman_numerals[2] * 2
-        #
-        # if 60 <= user_number <= 69 or 60 <= user_number % 100 <= 69:
-        #     converted_number += roman_numerals[2]
-        #
-        # if 50 <= user_number <= 59 or 50 <= user_number % 100 <= 59:
-        #     converted_number += roman_numerals[3]
-        #
-        # if 40 <= user_number <= 49 or 40 <= user_number % 100 <= 49:
-        #     converted_number += roman_numerals[2] + roman_numerals[3]
-        #
-        # print(converted_number)
+    elif temp_input_number == 8:
+        converted_number += roman_numerals[1 + index_add] + roman_numerals[0 + index_add]*3
+
+    elif temp_input_number == 7:
+        converted_number += roman_numerals[1 + index_add] + roman_numerals[0 + index_add]*2
+
+    elif temp_input_number == 6:
+        converted_number += roman_numerals[1 + index_add] + roman_numerals[0 + index_add]
+
+    elif temp_input_number == 5:
+        converted_number += roman_numerals[1 + index_add]
+
+    elif temp_input_number == 4:
+        converted_number += roman_numerals[0 + index_add] + roman_numerals[1 + index_add]
+
+    elif temp_input_number == 3:
+        converted_number += roman_numerals[0 + index_add]*3
+
+    elif temp_input_number == 2:
+        converted_number += roman_numerals[0 + index_add]*2
+
+    elif temp_input_number == 1:
+        converted_number += roman_numerals[0 + index_add]
+
+    if len(str(input_number)) > 1:
+        converted_number += roman_numeral_conversion(next_number)
+    return converted_number
+
+
 
 # Program Begins
 while True:
@@ -125,6 +114,9 @@ while True:
 
 
     elif menu_answer == 2:
+        user_number = int(input('What number would like converted?'))
+        print('The number you enter is written in roman numerals like this: ', end='')
+        print(roman_numeral_conversion(user_number))
 
 
 
