6774e50166e30edb2b14d24fd9e54a30a6de9f4c
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-18 16:24:35 -0700

    working on version 2

--- Code/Cheryl/lab02v2.py
+++ Code/Cheryl/lab02v2.py
@@ -4,7 +4,7 @@
 # Use string concatenation to put each word into the Mad Lib
 # Make a functional solution that utilizes lists. For example, ask the user for 3 adjectives, separated by commas, then use the .split() function to store each adjective and later use it in your story.
 # Add randomness! Use the random module, rather than selecting which adjective goes where in the story.
-
+import random
 welcome_mess = input("Thanks for visiting Cheryl's Mad Lib! Hit enter to play. ")
 
 verbs = []
@@ -29,4 +29,6 @@ names.append(input("Please enter another name. >    "))
 
 
 print(verbs, names, nouns, adjectives)
+
+print(f"Here's your Mad Lib! \nWhen {names[0]} starts to {random.choice(verbs)}\n with a {random.choice(adjectives)} {random.choice(nouns)}, {names[1]} \ncomes in and starts to {random.choice(verbs)} under the {random.choice(adjectives)} \n{random.choice(nouns)}. After all is said and\n {random.choice(verbs)}, the {random.choice(adjectives)} {random.choice(nouns)} \ntakes a nice, long {random.choice(verbs)}")
 
