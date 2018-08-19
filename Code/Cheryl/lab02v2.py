144f9b7a0346c1e623565bd651d5b2e74fc94c89
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-18 20:36:38 -0700

    working on version 2

--- Code/Cheryl/lab02v2.py
+++ Code/Cheryl/lab02v2.py
@@ -9,37 +9,41 @@ adjectives = []
 names = []
 
 #to see if I can get a list item to append itself into three different list items
+
+verbs.append(input("Please enter a list of verbs, separated by commas. >    "))
 for verb in verbs:
-    if verb[0] == ',':
+    if verb[0] == ',': #why does this work with letters but not commas
         print("* " + verb)
+print(verbs)
+
 ##############################
-
-verbs.append(input("Please enter a verb. >    "))
-verbs.append(input("Please enter another verb. >    "))
-verbs.append(input("Please enter a last verb. >    "))
-
-nouns.append(input("Please enter a noun. >    "))
-nouns.append(input("Please enter another noun. >    "))
-nouns.append(input("Please enter a last noun. >    "))
-
-adjectives.append(input("Please enter an adjective. >    "))
-adjectives.append(input("Please enter another adjective. >    "))
-adjectives.append(input("Please enter a last adjective. >    "))
-
-names.append(input("Please enter a name. >    "))
-names.append(input("Please enter another name. >    "))
-
-
-# {random.choice(verbs)}
+#
+# verbs.append(input("Please enter a verb. >    "))
+# verbs.append(input("Please enter another verb. >    "))
+# verbs.append(input("Please enter a last verb. >    "))
+#
+# nouns.append(input("Please enter a noun. >    "))
+# nouns.append(input("Please enter another noun. >    "))
+# nouns.append(input("Please enter a last noun. >    "))
+#
+# adjectives.append(input("Please enter an adjective. >    "))
+# adjectives.append(input("Please enter another adjective. >    "))
+# adjectives.append(input("Please enter a last adjective. >    "))
+#
+# names.append(input("Please enter a name. >    "))
+# names.append(input("Please enter another name. >    "))
+#
+#
+# # {random.choice(verbs)}
 # story = random.randint(1, 3)
-story = 2
-if story == 1:
-    print(f'\n{random.choice(nouns)} {random.choice(verbs)} {random.choice(nouns)}. \n{random.choice(nouns)} {random.choice(verbs)} {random.choice(nouns)}. \n{random.choice(nouns)} {random.choice(verbs)} {random.choice(nouns)}.\n-- {names[0]}\n\nWar is peace. \nFreedom is slavery. \nIgnorance is strength.\n-- George Orwell')
-
-elif story == 2:
-    print(f'The very concept of \n{random.choice(adjectives)} {random.choice(nouns)} {random.choice(verbs)} fading \nout of the {random.choice(nouns)}. \n{random.choice(verbs)} will pass into {random.choice(nouns)}.\n-- {names[1]}\n\nThe very concept of \nobjective truth is fading \nout of the world. \nLies will pass into history.\n-- George Orwell')
-
-else:
-    print('All animals are equal, \nbut some animals are more \nequal than others.\n-- George Orwell')
+#
+# if story == 1:
+#     print(f'\n{random.choice(nouns)} {random.choice(verbs)} {random.choice(nouns)}. \n{random.choice(nouns)} {random.choice(verbs)} {random.choice(nouns)}. \n{random.choice(nouns)} {random.choice(verbs)} {random.choice(nouns)}.\n-- {names[0]}\n\nWar is peace. \nFreedom is slavery. \nIgnorance is strength.\n-- George Orwell')
+#
+# elif story == 2:
+#     print(f'The very concept of \n{random.choice(adjectives)} {random.choice(nouns)} {random.choice(verbs)} fading \nout of the {random.choice(nouns)}. \n{random.choice(verbs)} will pass into {random.choice(nouns)}.\n-- {names[1]}\n\nThe very concept of \nobjective truth is fading \nout of the world. \nLies will pass into history.\n-- George Orwell')
+#
+# else:
+#     print(f'{random.choice(adjectives)} {random.choice(nouns)} are {random.choice(nouns)}, \nbut some {random.choice(adjectives)} {random.choice(verbs)} {random.choice(adjectives)} \n{random.choice(nouns)} than others.\n-- {name[1]}\n\nAll animals are equal, \nbut some animals are more \nequal than others.\n-- George Orwell')
 
 
