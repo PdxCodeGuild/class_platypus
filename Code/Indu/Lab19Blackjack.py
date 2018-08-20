88b043da895e9bbe6f8e2ab6f0f169aefafc1d0f
induts <induts@gmail.com>
2018-08-20 11:03:56 -0700

    Blackjack

--- Code/Indu/Lab19Blackjack.py
+++ Code/Indu/Lab19Blackjack.py
@@ -2,4 +2,14 @@ card = {'A':1,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':
 first_card = input("What's your first card? \n")
 second_card = input("What's your second card? \n")
 third_card = input("What's your third card? \n")
-sum =
+sum = card[first_card]+card[second_card]+card[third_card]
+print(sum)
+if sum < 17:
+    print("Hit")
+elif sum < 21 and sum >=17:
+    print("Stay")
+elif sum == 21:
+    print("Blackjack!!")
+else :
+    print("Busted")
+
