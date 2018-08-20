381bffacdbe07f9e77a5826901686cf193aac1d6
Skyler <me@skylerparker.com>
2018-08-20 10:41:37 -0700

    ...

--- Code/Sky/python_labs/lab19_blackjack_advice.py
+++ Code/Sky/python_labs/lab19_blackjack_advice.py
@@ -14,10 +14,19 @@ while True:
 
     if menu_answer == 1:
         card_values = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
-                       '9': 9, 'J': 10, 'Q': 11, 'K': 12}
+                       '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
         first_card = card_values[input('What\'s your first card?')]
         second_card = card_values[input('What\'s your second card?')]
         third_card = card_values[input('What\'s your second card?')]
+
+        total = first_card + second_card + third_card
+
+        if first_card == 'A' and total <= 11
+            total += 10
+        if second_card == 'A' and total <= 11
+            total += 10
+        if third_card == 'A' and total <= 11
+            total += 10
 
         print('\nYour current hand is worth: ' + str(first_card + second_card + third_card))
         if first_card + second_card + third_card > 21:
