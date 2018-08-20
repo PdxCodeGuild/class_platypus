03e37e7e00fda18dbc9bbe3dedb4d4151b18af5d
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-19 18:52:37 -0700

    finished version 1

--- Code/Cheryl/lab03V3.py
+++ Code/Cheryl/lab03V3.py
@@ -3,27 +3,28 @@
 user_grade = int(input("Please enter your number grade: > "))
 
 # to add + and -
-
 singles_digit = user_grade % 10
-if singles_digit < 5:
+if singles_digit < 4:
     prefix = '-'
-elif singles_digit > 5:
-    prefix = ""
+elif singles_digit > 6:
+    prefix = "+"
+else:
+    prefix = ''
 
 
 if user_grade >= 90:
-    print('Wahoo!!!That\'s an \'A\'' + prefix)
+    print(f'Wahoo!!!That\'s an A{prefix}')
 
 elif user_grade >= 80:
-    print('Nice job! That\'s a \'B\'' + prefix)
+    print(f'Nice job! That\'s a B{prefix}')
 
 elif user_grade >= 70:
-    print('Meh... That\'s a \'C\'' + prefix)
+    print(f'Meh... That\'s a C{prefix}')
 
 elif user_grade >= 60:
-    print('That\'s a \'D\'' + prefix)
+    print(f'That\'s a D{prefix}')
 
 else:
-    print('Hide your face! That\'s an \'F\'')
+    print('Hide your face! That\'s an F')
 
 
