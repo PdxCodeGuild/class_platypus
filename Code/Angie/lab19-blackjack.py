58592bae42e9380a80a4b930bd9327e554e24852
Aceome <angelina.bowmer@gmail.com>
2018-08-20 10:43:45 -0700

    lab19

---
+++ Code/Angie/lab19-blackjack.py
@@ -0,0 +1,19 @@
+cards = {'A': 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'J': 10, 'Q': 10, 'K': 10}
+
+x = input('what is your first card? ').upper()
+y = input('what is your second card? ').upper()
+z = input('what is your third card? ').upper()
+
+value = x + y + z
+
+print(value)
+
+#if value <= 17:
+#    print('Hit!')
+
+
+
+
+
+
+
