c9e3970241ed55f136630f2a7df5df3be83b52b4
induts <induts@gmail.com>
2018-08-20 10:59:34 -0700

    Blackjack

---
+++ Code/Indu/Lab19Blackjack.py
@@ -0,0 +1,5 @@
+card = {'A':1,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10}
+first_card = input("What's your first card? \n")
+second_card = input("What's your second card? \n")
+third_card = input("What's your third card? \n")
+sum =
