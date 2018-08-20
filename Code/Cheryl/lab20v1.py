3de1630d2b53a46b4922630ca13df7afc3c2ca5a
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-20 13:59:21 -0700

    version 1

--- Code/Cheryl/lab20v1.py
+++ Code/Cheryl/lab20v1.py
@@ -1,9 +1,24 @@
 #lab 20 version 1
 
-user_card_nums = input('What are the digits of your credit card? >  ').split()
+user_card_nums = input('What are the digits of your credit card? >  ')
 
-list(user_card_nums)
+user_card_nums = [int(x) for x in user_card_nums] #converts to an int
 
-user_card_nums.pop(-1)
+#pop off last num
+check_digit = user_card_nums.pop()
 
-print(user_card_nums)
+#reverse
+user_card_nums.reverse()
+
+#double every other element
+odd_nums = user_card_nums[1::2] #odd numbers
+double_nums = [x * 2 for x in odd_nums]
+print(f'{double_nums} + I\'m double')
+
+#subtract nine from numbers over 9
+i = 0
+while i < len(double_nums[i] > 9:
+    print(f'{double_nums[i] - 9} I am subtract 9')
+    i += 1
+
+# print(user_card_nums)
