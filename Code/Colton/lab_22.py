7c81dbc1f4c84b010fd80763e79ca0a0b90e58ea
ColtonJP <colton.peterson06@gmail.com>
2018-08-21 15:18:24 -0700

    updated

--- Code/Colton/lab_22.py
+++ Code/Colton/lab_22.py
@@ -1,41 +1,52 @@
 
 with open('wright_brothers.txt', 'r', encoding='utf-8') as f:
     contents = f.read()
-# find words in text
-def words_in_text():
-    contents = contents.lower()
-    punctuation = ['.', '(', ')', "'", '!', '@', '#', '-', ';', '/', ':', '$', '%', '^', '&', '*', '_', '`', '?', '"', ',', '\n', '\\', '’', '“', '”',]
-    for char in punctuation:
-        contents = contents.replace(char, ' ')
-    contents = contents.split()
-    print(contents)
-    word_dict = {}
-    for i in range(len(contents)):
-        if contents[i] not in word_dict:
-            word_dict[contents[i]] = 1
-        else:
-            word_dict[contents[i]] += 1
-    words = list(word_dict.items())
-    words.sort(key=lambda tup: tup[1], reverse=True)
-    for i in range(min(10, len(words))):
-        print(words[i])
-
-
-
-
+    # def words_in_text():
+    #     with open('wright_brothers.txt', 'r', encoding='utf-8') as f:
+    #         contents = f.read()
+    #     contents = contents.lower()
+    #     punctuation = "/[-[\]{}()*+?.,\\^$|#\s]/g, \"\\$&"
+    #     for char in punctuation:
+    #         contents = contents.replace(char, ' ')
+    #     contents = contents.split()
+    #     count = 0
+    #     for word in range(len(contents)):
+    #         count += 1
+    #     return count
+#
+#
+#
 # characters in text
 def char_in_text():
+    with open('wright_brothers.txt', 'r', encoding='utf-8') as f:
+        contents = f.read()
     contents = contents.lower()
-    punctuation = ['.', '(', ')', "'", '!', '@', '#', '-', ';', '/', ':', '$', '%', '^', '&', '*', '_', '`', '?', '"', ',', '\n', '\\', '’', '“', '”',' ']
+    punctuation = "/[-[\]{}()*+?.,\\^$|#\s]/g, \"\\$& "
     for char in punctuation:
         contents = contents.replace(char, '')
-    word_dict = {}
-    for i in range(len(contents)):
-        if contents[i] not in word_dict:
-            word_dict[contents[i]] = 1
-        else:
-            word_dict[contents[i]] += 1
-    words = list(word_dict.items())
-    words.sort(key=lambda tup: tup[1], reverse=True)
-    for i in range(min(10, len(words))):
-        return words[i]
+    contents = list(contents)
+    print(contents)
+    count = 0
+    for char in range(len(contents)):
+        count += 1
+    return count
+
+
+# sentences in a text
+# import re
+# contents = contents.lower()
+# punctuation = "/[-[\]{}()*+?.,\\^$|#\s]/g, \"\\$&"
+# for char in punctuation:
+#     contents = contents.replace(char, ' ')
+# contents = re.split(r' *[\.\?!][\'"\)\]]* *', contents)
+# print(contents)
+# word_dict = {}
+# for i in range(len(contents)):
+#     if contents[i] not in word_dict:
+#         word_dict[contents[i]] = 1
+#     else:
+#         word_dict[contents[i]] += 1
+# words = list(word_dict.items())
+# words.sort(key=lambda tup: tup[1], reverse=True)
+# for i in range(min(10, len(words))):
+#    print(words[i])
