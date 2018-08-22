02bd9a8206ae94a6a19b0ae097568d20b9c682ba
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-22 11:18:48 -0700

    working on version 1

--- Code/Cheryl/lab21v1.py
+++ Code/Cheryl/lab21v1.py
@@ -1,16 +1,11 @@
 #lab 21 version 1
 
-#represents current directory
-file.txt
 
 #to open a file
-f = open('Nietzsche.txt')  # open the file
+f = open('Nietzsche.txt', 'r')  # open the file
 contents = f.read()  # read the contents
 
-
-# step 1- to lowercase
-f = f.lower()
-
-print(contents)
+#to lowercase
+print(contents.lower())
 
 f.close()  # close the file
