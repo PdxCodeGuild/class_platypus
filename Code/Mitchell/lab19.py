c3a5778e0627c8d592745751cb0b504bcc7835de
Mitchell Estes <mestes1530@gmail.com>
2018-08-23 15:37:15 -0700

    Fixed spelling on print statement.

--- Code/Mitchell/lab19.py
+++ Code/Mitchell/lab19.py
@@ -2,7 +2,7 @@ cardValue = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9'
 firstCard = input('What\'s your first card? ').upper()
 secondCard = input('What\'s your second card? ').upper()
 thirdCard = input('What\'s your third card? ').upper()
-cardTotal = int(cardValue[firstCard] + cardValue[secondCard] + cardValue[thirdCard])
+cardTotal = cardValue[firstCard] + cardValue[secondCard] + cardValue[thirdCard]
 action = ''
 if cardTotal < 17:
     action = ' Hit'
@@ -11,5 +11,5 @@ elif cardTotal >= 17 and cardTotal < 21:
 elif cardTotal > 21:
     action = ' Already Busted'
 else:
-    action = ' BlacKjack!'
+    action = ' Blackjack!'
 print(str(cardTotal) + action)
