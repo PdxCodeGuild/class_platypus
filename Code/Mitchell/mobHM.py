8cdb59339b0021bcc0950c916d0170cb4768cd18
Mitchell Estes <mestes1530@gmail.com>
2018-08-23 14:56:42 -0700

    Added pictures to game.

--- Code/Mitchell/mobHM.py
+++ Code/Mitchell/mobHM.py
@@ -1,4 +1,83 @@
 import random
+hangman_pics = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+./|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+./|\. |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+./|\. |
+./ \  |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+./|\. |
+./ \. |
+      |
+=========''']
+pic = 0
 # Opens and copies the text of a book's .txt file
 with open('english.txt', 'r') as f:
     input_text = f.read()
@@ -27,6 +106,7 @@ while playing:
         board = board + '_ '
     # Loop until they player runs out of guesses
     while guesses > 0:
+        print(hangman_pics[pic])
         print(board)
         print('# of guesses remaining: ' + str(guesses))
         print('already guessed: ' + already_guessed)
@@ -45,11 +125,13 @@ while playing:
             if picked.find(letter_guess) == -1:
                 already_guessed = already_guessed + letter_guess + ', '
                 guesses -= 1
+                pic += 1
                 break
     # Prints win/lose message and play again option
     if won == True:
         print('You won!')
     else:
+        print(hangman_pics[10])
         print('You lost...')
     play_again = input('Do you want to play again? (type "yes" or "no") ').lower()
     if play_again == 'no':
