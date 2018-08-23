924e5fbda83a038bfd8048703db8600752d1e398
Mitchell Estes <mestes1530@gmail.com>
2018-08-23 14:28:52 -0700

    Working game missing play again option.

--- Code/Mitchell/mobHM.py
+++ Code/Mitchell/mobHM.py
@@ -11,32 +11,42 @@ while i < len(words):
         words.remove(words[i])
     else:
         i += 1
-# Randomly pick a word
-random.shuffle(words)
-picked = words.pop()
-print(picked)
+# Loop used while game is being plaid
+playing = True
+while playing:
+    # Randomly pick a word
+    random.shuffle(words)
+    picked = words.pop()
+    print(picked)
 
-# Set up the board
-guesses = 10
-already_guessed = ''
-board = ''
-# Initialize the board and pick word
-for i in range(len(picked)):
-    board = board + '_ '
-# Loop until they player runs out of guesses
-while guesses > 0:
-    print(board)
-    print('# of guesses remaining: ' + str(guesses))
-    print('already guessed: ' + already_guessed)
-    letter_guess = input('Guess a letter: ')
-    # Loop through picked word, see if guest letter is in it
+    # Set up the board
+    guesses = 10
+    already_guessed = ''
+    board = ''
+    # Initialize the board and pick word
     for i in range(len(picked)):
-        # If guessed letter is in word, replace '_' with it
-        if picked[i] == letter_guess:
-            i *= 2
-            board = board[:i] + letter_guess + board[i + 1:]
-        # If not in word, add it to already_guest and decrement guesses
-        if picked.find(letter_guess) == -1:
-            already_guessed = already_guessed + letter_guess + ', '
-            guesses -= 1
-            break
+        board = board + '_ '
+    # Loop until they player runs out of guesses
+    while guesses > 0:
+        print(board)
+        print('# of guesses remaining: ' + str(guesses))
+        print('already guessed: ' + already_guessed)
+        if board.find('_') == -1:
+            play_again = input('You Won! Do you want to play again? (Type "yes" or "no")').lower()
+            if play_again == 'no':
+                playing = False
+        letter_guess = input('Guess a letter: ').lower()
+        # Loop through picked word, see if guest letter is in it
+        for i in range(len(picked)):
+            # If guessed letter is in word, replace '_' with it
+            if picked[i] == letter_guess:
+                i *= 2
+                board = board[:i] + letter_guess + board[i + 1:]
+            # If not in word, add it to already_guest and decrement guesses
+            if picked.find(letter_guess) == -1:
+                already_guessed = already_guessed + letter_guess + ', '
+                guesses -= 1
+                break
+    play_again = input('Sorry you lost, want to play again? (Type "yes" or "no")').lower()
+    if play_again == 'no':
+        break
