9764e6362217978ac9661e5fdab359a7bbf81fbb
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-23 14:10:01 -0700

    still working

--- Code/Cheryl/hangman.py
+++ Code/Cheryl/hangman.py
@@ -13,20 +13,36 @@ def load_words(path):
 
 
 words = load_words('english.txt')
-secret = random.choice(words)
+# secret = random.choice(words)
+secret = "hippopotamus"
 print(secret)
-secret = list(secret)
+#secret = list(secret)
 #print(secret)
 print(f"The length of word you have to guess is {len(secret)}")
 already_guessed = []
 correct_letter = len(secret)*['_']
 print(' '.join(correct_letter))
 user_guess = input("Guess a letter?")
-if user_guess in secret:
-    index = index(user_guess in secret)
-    print(index)
 
 
+while True:
+    count=0
+    if  user_guess in secret:
+        word_index = secret.index(user_guess)
+        print(word_index)
+        correct_letter.insert(word_index,user_guess)
+        print(' '.join(correct_letter))
+        already_guessed.append(user_guess)
+        secret[secret.index(user_guess) + 1:]
+        # if user_guess in secret:
+
+            # secret[secret.index_one(user_guess) + 1:].index_one(user_guess)
+        # secret[secret.index(user_guess) + 1:].index(user_guess) + secret.index(user_guess) + 1
+
+    else:
+
+        count +=1
+        print(f"not a letter in the word. You have {10-count} guesses left")
 
 
 
