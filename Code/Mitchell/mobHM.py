cc39c8b6683bfa73b9c1ea68c60b16aa528b634f
Mitchell Estes <mestes1530@gmail.com>
2018-08-23 12:17:43 -0700

    Stuck in loop.

--- Code/Mitchell/mobHM.py
+++ Code/Mitchell/mobHM.py
@@ -24,14 +24,20 @@ for i in range(len(picked)):
     board = board + '_ '
 # Play the game
 while guesses > 0:
-    print(board)
     print('# of guesses remaining: ' + str(guesses))
+    print('already guessed: ' + already_guessed)
     letter_guess = input('Guess a letter: ')
     if picked.find(letter_guess) == -1:
         already_guessed = already_guessed + letter_guess
         guesses -= 1
     else:
-        board[picked.find(letter_guess)] = letter_guess
+        board_spot = picked.find(letter_guess) * 2
+        board = board[:board_spot] + letter_guess + board[board_spot+1:]
+        other_spot = board_spot + 1
+        while picked.find(letter_guess) * 2 != -1:
+        #for other_spot in range(len(board)):
+            other_spot = picked.find(letter_guess) * 2
+            board = board[:other_spot] + letter_guess + board[other_spot + 1:]
     print(board)
 
 
