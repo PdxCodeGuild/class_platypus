61e86128562ed84d3450419e368ee53eba233f8d
Mitchell Estes <mestes1530@gmail.com>
2018-08-23 11:23:21 -0700

    Stuggling with else.

--- Code/Mitchell/mobHM.py
+++ Code/Mitchell/mobHM.py
@@ -3,10 +3,35 @@ import random
 with open('english.txt', 'r') as f:
     input_text = f.read()
 f.close()
-
 words = list(input_text.split('\n'))
-for i in range(len(words)):
+# Removes all words with less than 5 letters
+i = 0
+while i < len(words):
     if len(words[i]) < 5:
         words.remove(words[i])
-        continue
-print(words)
+    else:
+        i += 1
+# Randomly pick a word
+random.shuffle(words)
+picked = words.pop()
+print(picked)
+
+# Set up the board
+guesses = 10
+already_guessed = ''
+board = ''
+for i in range(len(picked)):
+    board = board + '_ '
+# Play the game
+while guesses > 0:
+    print(board)
+    print('# of guesses remaining: ' + str(guesses))
+    letter_guess = input('Guess a letter: ')
+    if picked.find(letter_guess) == -1:
+        already_guessed = already_guessed + letter_guess
+        guesses -= 1
+    else:
+        board[picked.find(letter_guess)] = letter_guess
+    print(board)
+
+
