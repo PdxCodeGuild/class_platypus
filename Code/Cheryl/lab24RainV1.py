0f26101b17157954c78f23bf7e18a477b2deb7e6
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-24 10:42:38 -0700

    still working

--- Code/Cheryl/lab24RainV1.py
+++ Code/Cheryl/lab24RainV1.py
@@ -1,18 +1,56 @@
 #lab 24 version 1
 
-import datetime
+########################################
+
+import requests
 import re
-
-with open('rain_data.txt', 'r') as f:
-    contents = f.read()
-
-#trying to figure out regex
-matchObject = re.search(' ', contents, flags=0)
-print(matchObject)
+import datetime
+import ast
 
 #
+# url = 'https://or.water.usgs.gov/non-usgs/bes/'
 #
-# date = datetime.datetime.strptime('25-MAR-2016', '%d-%b-%Y')
+#
+# def get_locations():
+#     r = requests.get(url)
+#     locations = re.findall(r'\w+\.rain', r.text)
+#     locations.sort()
+#     return locations
+#
+#
+# def get_file(location):
+#     r = requests.get(url + location)
+#     return r.text
+#
+#
+# locations = get_locations()
+#
+# print('which location would you like to see?')
+# for i in range(len(locations)):
+#     print(f'\t{i} {locations[i]}')
+#
+# location_index = int(input('> '))
+#
+# print(get_file(locations[location_index]))
+
+########################################################################
+
+
+def get_date(path):
+    with open(path, 'r') as f:
+        contents = f.read()
+
+    date = re.findall(r'\d\d\-\D\D\D\-\d\d\d\d', contents)
+    date = ' '.join(date)
+
+    return date
+date_dict = {(get_date('rain_test.txt'))}
+print(get_date('rain_test.txt'))
+print(date_dict)
+
+
+#
+# date = datetime.datetime.strptime(get_date('rain_test.txt'), '%d-%b-%Y')
 # print(date.year)   # 2016
 # print(date.month)  # 3
 # print(date.day)    # 25
