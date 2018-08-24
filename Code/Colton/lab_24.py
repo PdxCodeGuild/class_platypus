b5093ecd5098ff5e0897cceb173c34eadbd86da4
ColtonJP <colton.peterson06@gmail.com>
2018-08-24 16:22:02 -0700

    updated

--- Code/Colton/lab_24.py
+++ Code/Colton/lab_24.py
@@ -5,7 +5,7 @@ with open('hayden_island.txt', 'r') as f:
 # use regex to find the dates from the text
 dates = re.findall(r'[0-9]+-[A-Z]+-[0-9]+', text)
 date = [datetime.datetime.strptime(i, '%d-%b-%Y') for i in dates]
-print(date[6].year)
+
 
 # use regex to find the total daily rain from the text then turn it into int's and take out extra spaces
 totals = re.findall('(?:\d{4})( +[0-9]+)', text)
@@ -14,7 +14,6 @@ totals = [int(i) for i in totals]
 
 
 # take the two lists and make them a dict.
-# dates_totals_dict = dict(zip(date, totals))
 dates_totals_dict = []
 for i in range(len(totals)):
     dates_totals_dict.append({
@@ -22,10 +21,15 @@ for i in range(len(totals)):
         'daily_total': totals[i]
     })
 
+print(dates_totals_dict)
+print(dates_totals_dict[1]['date']['daily_total'])
+
+
+
+
+
 # find the mean of the whole thing
 import statistics
-numbers = [dates_totals_dict[key] for key in dates_totals_dict]
+numbers = [dates_totals_dict['daily_total'] for ['daily_total'] in dates_totals_dict['daily_total']]
 mean_ = statistics.mean(numbers)
 print(mean_)
-
-
