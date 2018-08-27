6e354c873ef00bfbce3f32117325408edf266fc3
ColtonJP <colton.peterson06@gmail.com>
2018-08-27 15:45:18 -0700

    updated

--- Code/Colton/lab_24.py
+++ Code/Colton/lab_24.py
@@ -28,22 +28,62 @@ import statistics
 numbers = [dates_totals_dict[i]['daily_total'] for i in range(len(dates_totals_dict))]
 avg = statistics.mean(numbers)
 
-#avg rainfall for 2018
-dt_2018 = 0
-count_2018 = 0
-for row in dates_totals_dict:
-    if row['date'].year == 2018:
-        dt_2018 += row['daily_total']
-        count_2018 += 1
-print(dt_2018/count_2018)
+# #avg rainfall for 2018
+# dt_2018 = []
+# count_2018 = 0
+# for row in dates_totals_dict:
+#     if row['date'].year == 2018:
+#         dt_2018.append(row['daily_total'])
+#         count_2018 += 1
 
+
+
+years = list({date.year for date in dates})
+months = []
+for year in years:
+    for month in range(12):
+        months.append((year, month))
+print(months)
+
+monthly_totals = [0]*len(months)
+count = 0
+for i in range(len(months)):
+    if months[i] == months[i]:
+        monthly_totals += totals['daily_totals']
+        count += 1
+print(monthly_totals)
+
+
+
+
+# month_total = 0
+# month_list = []
+# count = 0
+# for row in dates_totals_dict:
+#     if row['date'].month == row['date'].month - 1:
+#         month_total += row['daily_total']
+#         count += 1
+#     else:
+#         month_total = month_total
+#         month_list.append(row['date'].month)
+#
+# print(month_list)
+# print(month_total)
+# print(count)
 # plot of all data over the years
-import matplotlib.pyplot as plt
-x_values = [dates_totals_dict[i]['date'] for i in range(len(dates_totals_dict))]
-y_values = [dates_totals_dict[i]['daily_total'] for i in range(len(dates_totals_dict))]
-plt.plot(x_values, y_values)
-plt.show()
+# import matplotlib.pyplot as plt
+# x_values = [dates_totals_dict[i]['date'] for i in range(len(dates_totals_dict))]
+# y_values = [dates_totals_dict[i]['daily_total'] for i in range(len(dates_totals_dict))]
+# plt.plot(x_values, y_values)
+# plt.show()
 
-
+# plot of rainfall through 2018
+# import matplotlib.pyplot as plt
+# x_values = range(count_2018)
+# y_values = dt_2018
+# plt.xlabel('Days through the year')
+# plt.ylabel('amount of rainfall')
+# plt.plot(x_values, y_values)
+# plt.show()
 
 
