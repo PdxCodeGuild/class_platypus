6dd671a64aa41b47131ea340ef5338ae7b897537
ColtonJP <colton.peterson06@gmail.com>
2018-08-27 14:18:48 -0700

    updated

--- Code/Colton/lab_24.py
+++ Code/Colton/lab_24.py
@@ -27,7 +27,23 @@ import statistics
 # # find the mean of the whole thing in tips
 numbers = [dates_totals_dict[i]['daily_total'] for i in range(len(dates_totals_dict))]
 avg = statistics.mean(numbers)
-print(avg)
 
-var = sum((row - avg) ** .5 for row in dates_totals_dict) / len(dates_totals_dict)
-print(var)
+#avg rainfall for 2018
+dt_2018 = 0
+count_2018 = 0
+for row in dates_totals_dict:
+    if row['date'].year == 2018:
+        dt_2018 += row['daily_total']
+        count_2018 += 1
+print(dt_2018/count_2018)
+
+# plot of all data over the years
+import matplotlib.pyplot as plt
+x_values = [dates_totals_dict[i]['date'] for i in range(len(dates_totals_dict))]
+y_values = [dates_totals_dict[i]['daily_total'] for i in range(len(dates_totals_dict))]
+plt.plot(x_values, y_values)
+plt.show()
+
+
+
+
