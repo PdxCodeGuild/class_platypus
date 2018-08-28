dab07d51900f7e889189c69fc67b903dfd5982c5
Mitchell Estes <mestes1530@gmail.com>
2018-08-28 11:10:37 -0700

    Added a function to print/keep track of users history.

--- Code/Mitchell/lab25.py
+++ Code/Mitchell/lab25.py
@@ -1,6 +1,6 @@
 class ATM:
     # Initializes ATM account no history & balance of 0
-    def __init__(self, balance = 0, history = ''):
+    def __init__(self, balance = 0, history = []):
         self.balance = balance
         self.history = history
     # Method that checks the current balance of ATM
@@ -9,6 +9,7 @@ class ATM:
     # Deposits passed amount into ATM account
     def deposit(self, amount):
         self.balance += amount
+        self.history.append('User deposited $' + str(amount))
     # Returns true if amount can be withdrawn from balance
     def check_withdrawal(self, amount):
         if amount <= self.balance:
@@ -19,11 +20,19 @@ class ATM:
     def withdraw(self, amount):
         if self.check_withdrawal(amount):
             self.balance -= amount
+            self.history.append('User withdrew $' + str(amount))
         else:
             print('ERROR: Withdraw amount is larger than current balance.')
     # Calculates amount of interest on ATM account
     def calc_interest(self):
         return self.balance + (self.balance * 0.001)
+    # Prints list of all transactions performed
+    def print_transactions(self):
+        print('Transaction History')
+        print('-------------------')
+        for i in range(len(self.history)):
+            print(self.history[i])
+        print('-------------------')
 # Print statements for debugging
 atm = ATM()
 print(atm.check_balance())  # "your balance is $0"
@@ -32,3 +41,4 @@ print(atm.check_balance())  # "your balance is $5"
 print(atm.check_withdrawal(10))  # False
 print(atm.withdraw(2))  # 2
 print(atm.check_balance())  # "your balance is $3"
+print(atm.print_transactions())
