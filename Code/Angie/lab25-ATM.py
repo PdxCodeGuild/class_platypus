a6446aada3efcecbe3305477dbd8b2e0b7f74792
Aceome <angelina.bowmer@gmail.com>
2018-08-28 12:30:33 -0700

    lab25

--- Code/Angie/lab25-ATM.py
+++ Code/Angie/lab25-ATM.py
@@ -2,35 +2,50 @@
 
 
 class ATM:
-    def __init__(self, balance, interest):
+    def __init__(self, balance=0, interest=0.001):
         self.balance = balance
         self.interest = interest
 
-    def check_balance(balance):
+    def check_balance(self):  # returns account balance
+        return f'Your balance is currently at ${self.balance}'
+
+    # def print_transactions(self):  # maintains a list of transactions
+    #     transact = []
+    #     for deposit in :
+    #         transact.append(deposit)
+    #
+    #
+
+    def deposit(self, amount):  # deposits the given amount into account
+        self.balance += amount
+
+    def check_withdrawal(self, amount):  # returns true if the withdrawn amount wont put the account in the negative
+        self.balance -= amount <= 0
+        return False
+
+    def withdraw(self, amount):  # withdraws the amount from the account and returns it
         pass
 
-    def deposit(amount):
-        pass
-
-    def check_withdrawal(amount):
-        pass
-
-    def withdraw(amount):
-        pass
-
-    def calc_interest(interest):
-        interest = (balance * .001) + balance
-
-    return calc_interest()
+    # def calc_interest(self, interest):  # returns the amount of interest
+    #     interest = (balance * .001) + balance
 
 
-atm = ATM(0, .001)
+
+
+atm = ATM()
 print(atm.check_balance())  # "your balance is $0"
 atm.deposit(5)
 print(atm.check_balance())  # "your balance is $5"
 print(atm.check_withdrawal(10))  # False
-print(atm.withdraw(2))  # 2
-print(atm.check_balance())  # "your balance is $3"
-
-
-
+# print(atm.withdraw(2))  # 2
+# print(atm.check_balance())  # "your balance is $3"
+#
+#
+# while True:
+#     user = input('what would you like to do (deposit, withdraw, check balance, history)?')
+# deposit
+# how much would you like to deposit?
+# $5
+# what would you like to do (deposit, withdraw, check balance, history)?
+# check balance
+# balance: $5
