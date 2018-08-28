e3a5c5c763c0a75533ddef1822b68a62fa71295e
Mitchell Estes <mestes1530@gmail.com>
2018-08-28 10:50:58 -0700

    Implemented ATM class with basic functions for version 1.

---
+++ Code/Mitchell/lab25.py
@@ -0,0 +1,34 @@
+class ATM:
+    # Initializes ATM account no history & balance of 0
+    def __init__(self, balance = 0, history = ''):
+        self.balance = balance
+        self.history = history
+    # Method that checks the current balance of ATM
+    def check_balance(self):
+        return self.balance
+    # Deposits passed amount into ATM account
+    def deposit(self, amount):
+        self.balance += amount
+    # Returns true if amount can be withdrawn from balance
+    def check_withdrawal(self, amount):
+        if amount <= self.balance:
+            return True
+        else:
+            return False
+    # Withdraws passed amount from ATM balance
+    def withdraw(self, amount):
+        if self.check_withdrawal(amount):
+            self.balance -= amount
+        else:
+            print('ERROR: Withdraw amount is larger than current balance.')
+    # Calculates amount of interest on ATM account
+    def calc_interest(self):
+        return self.balance + (self.balance * 0.001)
+# Print statements for debugging
+atm = ATM()
+print(atm.check_balance())  # "your balance is $0"
+atm.deposit(5)
+print(atm.check_balance())  # "your balance is $5"
+print(atm.check_withdrawal(10))  # False
+print(atm.withdraw(2))  # 2
+print(atm.check_balance())  # "your balance is $3"
