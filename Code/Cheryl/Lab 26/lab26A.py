6d8a8b8590c9c53113a54509096c7e7d562d999d
Cheryl Lewman <cheryllewman@gmail.com>
2018-08-30 16:22:14 -0700

    dealt with all of the options on how the game might end.

--- Code/Cheryl/Lab 26/lab26A.py
+++ Code/Cheryl/Lab 26/lab26A.py
@@ -66,8 +66,10 @@ class Enemy(Entity):
     def __init__(self, location_i, location_j):
         if user_enemy == "BORG":
             super().__init__(location_i, location_j, '🕋') #borg
+
         else:
             super().__init__(location_i, location_j, '👿') #romulan
+
 
 
 class Player(Entity):
@@ -128,13 +130,13 @@ entities = [player]
 enemies = []
 shields = []
 
-for i in range(10):
+for i in range(3):
     ei, ej = board.random_location()
     enemy = Enemy(ei, ej)
     entities.append(enemy)
     enemies.append(enemy)
 
-for i in range(5):
+for i in range(1):
     ei, ej = board.random_location()
     shield = Shield(ei, ej)
     entities.append(shield)
@@ -144,7 +146,7 @@ for i in range(5):
 #sets points for game
 points = 5
 new_points = points
-
+enemy_count = 0
 
 while True:
 
@@ -172,14 +174,29 @@ while True:
             audio('st_audio/tactalertvesselapproach.wav')
             print('you\'ve encountered an enemy!')
             action = input('what will you do? ')
+            # enemy_count = 0
             if action == 'attack':
+                enemy_count += 1
                 #adds random weapon sounds on attack
                 attack_audio = ["st_audio/tng_weapons.wav", "st_audio/tos_photon_torpedo.wav", "st_audio/tng_torpedo.wav", "st_audio/tng_torpedo2.wav", "st_audio/tng_torpedo3.wav"]
                 random_attack_audio = random.choice(attack_audio)
                 audio(random_attack_audio)
+                print(enemy_count)
+                if enemy_count == 3:
+                    print('You\'ve won the game! ')
+                    if user_race == 'KLINGON':
+                        audio('st_audio/largeexplosion.wav')
+                        print(style_files('klingon_ship.txt'))
+                        end_credits(audio)
+                        exit()
+                    else:
+                        audio('st_audio/livelong.wav')
+                        print(style_files('federation_ship.txt'))
+                        end_credits(audio)
+                        exit()
 
                 #adds points or deducts random points based on who wins the attack
-                win_lose = 'LOSE'
+                win_lose = 'WIN'
                 # win_lose = ['WIN', 'LOSE']
                 # win_lose = random.choice(win_lose)
                 if win_lose == 'LOSE':
@@ -191,7 +208,7 @@ while True:
                     elif new_points == 5:
                         audio('st_audio/lifesupportfailureabandon.wav')
                     if new_points == 0:
-                    #     ########### INSERT LOSING FUNCTION ###############
+                    # IF YOU LOSE
                         audio('st_audio/largeexplosion.wav')
                         if user_enemy == "BORG":
                             print('You have been assimilated.')
@@ -218,18 +235,23 @@ while True:
                 entities.remove(enemy)
                 enemies.remove(enemy)
                 break
+
+            elif enemy_count == 0:
+                print('WORKING')
             else:
                 print('you hesitated and were slain')
                 if user_enemy == "BORG":
                     print('You have been assimilated.')
                     time.sleep(3)
                     print(style_files('borg.txt') + '\t\tYou are now Borg...')
+                    end_credits(audio)
                 else:
                     if user_race == 'KLINGON':
                         print(style_files('romulan.txt') + '\t\tYou have been put in a Romulan Prison Camp where you will die a slow and dishonorable death.')
                     else:
                         print(style_files('romulan.txt') + 'test')
-
+                        end_credits(audio)
+                end_credits(audio)
                 exit()
 
 
