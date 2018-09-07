ad45ebbbf0ba437308fcabdab91f53fb1e40e00f
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-07 15:36:26 -0700

    It's working. Just need to add the other options.

--- Code/Cheryl/Python/pythonPortfolio/lab7REDO.py
+++ Code/Cheryl/Python/pythonPortfolio/lab7REDO.py
@@ -7,24 +7,26 @@ print('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
 
 #create function
 def game():
-    choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-    user_choice = input('Please choose rock, paper, scissors, lizard, or Spock. >  ').lower()
-    computer_choice = random.choice(choices)
+    play = 'yes'
+    while play == 'yes':
+        choices = ['rock', 'paper', 'scissors']
+        computer_choice = random.choice(choices)
+        user_choice = input('Please choose rock, paper, scissors, lizard, or Spock. >  ').lower()
+        rps_dict = {'rock': {'paper': True, 'scissors': False}, 'paper': {'rock': False, 'scissors': True}}
 
-    #indexes 0-1 lose, indexes 2-3 win
-    rps_dict = {'rock': ['paper', 'spock', 'scissors', 'lizard'], 'paper': ['scissors', 'lizard', 'spock', 'rock'], 'scissors': ['rock', 'spock', 'paper', 'lizard'], 'lizard': ['rock', 'scissors', 'spock', 'paper'], 'spock': ['lizard', 'paper', 'rock', 'scissors']}
+        print(f'\tYou chose: {user_choice.upper()}.\n\tThe computer chose: {computer_choice.upper()}.')
 
-
-    print(f'\tYou chose: {user_choice.upper()}.\n\tThe computer chose: {computer_choice.upper()}.')
-
-    # if rps_dict[user_choice]:
-    #     print(f'HELLLLOOOOOO + {user_choice}')
-
-    if user_choice in rps_dict:
-        if computer_choice in rps_dict[user_choice]:
-            print(rps_dict[user_choice])
-
-
+        if computer_choice == user_choice:
+            print(f'\tBoth you and the computer chose {user_choice}, so it\'s a tie.')
+        else:
+            bad_name = rps_dict[user_choice][computer_choice]
+            if bad_name == False:
+                print(f'\t{user_choice.upper()} beats {computer_choice.upper()}. You WIN!')
+            else:
+                print(f'\t{computer_choice.upper()} beats {user_choice.upper()}. You Lose.')
+        play = input('Would you like to play again? ').lower()
 
 
 print(game())
+print('Thanks for playing!')
+
