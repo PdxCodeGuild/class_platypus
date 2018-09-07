ee842bfd31548a845935426bfc5de10f3076d8c5
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-07 14:05:37 -0700

    made one dictionary, amended the function.

--- Code/Cheryl/Python/pythonPortfolio/lab7REDO.py
+++ Code/Cheryl/Python/pythonPortfolio/lab7REDO.py
@@ -2,14 +2,29 @@ import random
 
 
 print('Welcome to Rock, Paper, Scissors, Lizard, Spock!')
-choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-user_choice = input('Please choose rock, paper, scissors, lizard, or spock. >  ').lower()
-computer_choice = random.choice(choices)
 
-#indexes 0-1 lose, indexes 2-3 win
-rock_dict = {'rock': ['paper', 'spock', 'scissors', 'lizard']}
-paper_dict = {'paper': ['scissors', 'lizard', 'spock', 'rock']}
-scissors_dict = {'scissors': ['rock', 'spock', 'paper', 'lizard']}
-lizard_dict = {'lizard': ['rock', 'scissors', 'spock', 'paper']}
-spock_dict = {'spock': ['lizard', 'paper', 'rock', 'scissors']}
 
+
+#create function
+def game():
+    choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+    user_choice = input('Please choose rock, paper, scissors, lizard, or Spock. >  ').lower()
+    computer_choice = random.choice(choices)
+
+    #indexes 0-1 lose, indexes 2-3 win
+    rps_dict = {'rock': ['paper', 'spock', 'scissors', 'lizard'], 'paper': ['scissors', 'lizard', 'spock', 'rock'], 'scissors': ['rock', 'spock', 'paper', 'lizard'], 'lizard': ['rock', 'scissors', 'spock', 'paper'], 'spock': ['lizard', 'paper', 'rock', 'scissors']}
+
+
+    print(f'\tYou chose: {user_choice.upper()}.\n\tThe computer chose: {computer_choice.upper()}.')
+
+    # if rps_dict[user_choice]:
+    #     print(f'HELLLLOOOOOO + {user_choice}')
+
+    if user_choice in rps_dict:
+        if computer_choice in rps_dict[user_choice]:
+            print(rps_dict[user_choice])
+
+
+
+
+print(game())
