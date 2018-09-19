43b65d4682df7958f15ff5bb02033734f7dfb684
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-19 14:09:52 -0700

    finished part one of lab 4

---
+++ Code/Cheryl/JavaScript/lab4.js
@@ -0,0 +1,13 @@
+alert('test1')
+
+
+addItem = 'yes'
+if (addItem === 'yes'){
+    let userList = prompt('What item would you like to add? ')
+    userList.push(prompt('What other item would you like to add? '))
+    document.write('test4')
+}
+alert('test2')
+
+alert('test3')
+document.write(groceryList)
