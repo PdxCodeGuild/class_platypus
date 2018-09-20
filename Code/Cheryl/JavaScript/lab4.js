0ddc4cc7ff7e53143f80999cb5ae90811aaa992a
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-20 09:34:37 -0700

    can add and remove list items. need to figure out how to strikethrough.

--- Code/Cheryl/JavaScript/lab4.js
+++ Code/Cheryl/JavaScript/lab4.js
@@ -2,8 +2,9 @@ const addItemInput = document.querySelector('input.addItemInput')
 const addItemButton = document.querySelector('button.addItemButton')
 const removeItemInput = document.querySelector('input.removeItemInput')
 const removeItemButton = document.querySelector('button.removeItemButton')
-const strikeItemInput = document.querySelector('input.strikeItemInput')
+const strikeItemInput = document.getElementsByTagName('li')
 const strikeItemButton = document.querySelector('button.strikeItemButton')
+
 
 addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
@@ -11,6 +12,26 @@ addItemButton.addEventListener('click', () => {
   li.textContent = addItemInput.value;
   ul.appendChild(li);
   addItemInput.value = '';
+})
+
+// function strike() {
+//   if (strikeItemInput != ''){
+//     strikeItemInput.style.textDecoration = 'line-through';
+//   }
+// }
+
+// strikeItemButton.addEventListener('click', () => {
+//   let ul = document.getElementsByTagName('ul');
+//   let li = document.createElement('li');
+//   strikeItemInput[strike()].style.textDecoration = 'line-through';
+//   strikeItemInput.value = '';
+// })
+
+strikeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.createElement('li');
+  strikeItemInput[0].style.textDecoration = 'line-through';
+  strikeItemInput.value = '';
 })
 
 removeItemButton.addEventListener('click', () => {
