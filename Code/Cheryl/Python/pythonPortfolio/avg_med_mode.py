d12075bd71376817f12da3fbed1da1d62b90076b
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-25 13:17:32 -0700

    I think it's done!

--- Code/Cheryl/Python/pythonPortfolio/avg_med_mode.py
+++ Code/Cheryl/Python/pythonPortfolio/avg_med_mode.py
@@ -1,19 +1,21 @@
 from statistics import mode
 
+welcome_mess = print(f"\nWelcome to the Average, Median, and Mode Converter!\n")
 final_data_set = []
-data_set  = input('Please enter your data: ')
+data_set  = input('Please enter your data, separated by commas: ')
 data_set = data_set.split(',')
 
 for i in range(len(data_set)):
     i = data_set[i].strip(' ')
     final_data_set.append(i)
 
+
 # average
 def average():
     final_avg = [int(i) for i in final_data_set]
     final_avg = sum(final_avg)
     average = final_avg / len(final_data_set)
-    print(f'The average is: {average}')
+    print(f'The AVERAGE is: {average}')
     return
 
 average()
@@ -26,7 +28,7 @@ def median():
     if len(final_data_set) % 2 > 0:
         median_index = len(final_data_set) // 2
         median = final_data_set[median_index]
-        print(f'The median number is: {median}')
+        print(f'The MEDIAN number is: {median}')
 
     else:
         median_index = len(final_data_set) // 2
@@ -38,9 +40,8 @@ def median():
 
 median()
 
+
 #mode
-
-
 def data():
     try:
 
