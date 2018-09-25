a0a9be7dff79bb43e69e5eab22afe3b14e38197f
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-24 17:25:30 -0700

    fixed it up a bit. Seems like it works as expected.

--- Code/Cheryl/Python/lab10V1.py
+++ Code/Cheryl/Python/lab10V1.py
@@ -4,9 +4,8 @@ nums = [5, 0, 8, 3, 4, 1, 6]
 running = 0
 
 for num in nums:
-    print(running)
     running += num
+    print(running)
 
-# average = sum(nums) / len(nums)
 average = running / len(nums)
-print(f'The floor division average is: {average}')
+print(f'The average is: {average}')
