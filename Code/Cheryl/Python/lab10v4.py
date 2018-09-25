3bde574deb4423a74796ee1f69af5fd04a9ff00d
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-25 06:53:00 -0700

    working for numbers but not strings...

--- Code/Cheryl/Python/lab10v4.py
+++ Code/Cheryl/Python/lab10v4.py
@@ -4,9 +4,15 @@ from statistics import mode
 from fractions import Fraction as fr
 
 def data():
-    user_input = input('Please enter your data set:   ')
-    print("Mode of data set 1 is % s" % (mode(user_input)))
-    return
+    try:
+        data_set = input('Please enter your data set. >  ')
+        data_set = data_set.split(',')
+        if data_set == str:
+            print('it is a string')
+        print("Mode of data set is % s" % (mode(data_set)))
+        return
+    except ValueError:
+        print('There is no mode in this data set.')
 
 data()
 
