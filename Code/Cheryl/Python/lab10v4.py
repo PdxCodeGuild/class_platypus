06c9fd3c9137945fee0f8b41f13603f534eabf25
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-25 11:24:11 -0700

    changed it to not require import statistics and now it works with strings instead of just numbers.

--- Code/Cheryl/Python/lab10v4.py
+++ Code/Cheryl/Python/lab10v4.py
@@ -1,17 +1,38 @@
 #lab 10 version 4 MODE
-from statistics import mode
 
-def data():
-    try:
-        final_data_set = []
-        data_set = input('Please enter your data set. >  ')
-        data_set = data_set.split(',')
-        for i in range(len(data_set)):
-            i = data_set[i].strip(' ')
-            final_data_set.append(i)
-        print(f"MODE of this data set is: {mode(final_data_set)}")
-        return
-    except ValueError:
-        print(f'There is no MODE in this data set: {data_set}')
 
-data()
+data_list = input('Input your list: ')
+for data in data_list:
+    data_dict = {}
+for data in data_list:
+    if data not in data_dict.keys():
+        data_dict[data] = 1
+
+
+data_dict = {}
+for data in data_list:
+    if data not in data_dict.keys():
+        data_dict[data] = 1
+    else:
+        data_dict[data] += 1
+
+
+
+# Another method that works
+#
+# from statistics import mode
+
+# def data():
+#     try:
+#         final_data_set = []
+#         data_set = input('Please enter your data set. >  ')
+#         data_set = data_set.split(',')
+#         for i in range(len(data_set)):
+#             i = data_set[i].strip(' ')
+#             final_data_set.append(i)
+#         print(f"MODE of this data set is: {mode(final_data_set)}")
+#         return
+#     except ValueError:
+#         print(f'There is no MODE in this data set: {data_set}')
+#
+# data()
--- Code/Cheryl/Python/pythonPortfolio/avg_med_mode.py
+++ Code/Cheryl/Python/pythonPortfolio/avg_med_mode.py
@@ -1,26 +1,32 @@
+from statistics import mode
+
+final_data_set = []
 data_set  = input('Please enter your data: ')
+data_set = data_set.split(',')
+
+for i in range(len(data_set)):
+    i = data_set[i].strip(' ')
+    final_data_set.append(i)
+
 # average
 def average():
     nums = []
     running = 0
-    answer = ''
 
+    try:
+        # data_set = input('Please enter your number, or type \'done\' to get the average. >    ')
+        # data_set = int(data_set)
+        # data_set = round(data_set, 2)
+        nums.append(data_set)
+        print(nums)
 
-    while data_set != 'done':
-        try:
-            # data_set = input('Please enter your number, or type \'done\' to get the average. >    ')
-            data_set = int(data_set)
-            data_set = round(data_set, 2)
-            nums.append(data_set)
-            print(nums)
-
-        except ValueError:
-            for num in nums:
-                running += num
-                running = round(running, 2)
-            print('Adding your numbers now...')
-            average = running / len(nums)
-            print(f'The average is: {average}')
+    except ValueError:
+        for num in nums:
+            running += num
+            running = round(running, 2)
+        print('Adding your numbers now...')
+        average = running / len(nums)
+        print(f'The average is: {average}')
 
     return
 
@@ -34,28 +40,27 @@ def median():
     median = ''
 
 
-    while data_set != 'done':
-        try:
-            # data_set = input('Please enter your number, or type \'done\' to get the median. >    ')
-            data_set = int(data_set)
-            data_set = round(data_set, 2)
-            nums.append(data_set)
-            print(nums)
+    try:
+        # data_set = input('Please enter your number, or type \'done\' to get the median. >    ')
+        # data_set = int(data_set)
+        # data_set = round(data_set, 2)
+        nums.append(data_set)
+        print(nums)
 
-        except ValueError:
+    except ValueError:
 
-            nums.sort()
-            length = len(nums)
+        nums.sort()
+        length = len(nums)
 
-            if len(nums) % 2 > 0:
-                median_index = len(nums) // 2
-                median = nums[median_index]
-                print(f'The median number is: {median}')
+        if len(nums) % 2 > 0:
+            median_index = len(nums) // 2
+            median = nums[median_index]
+            print(f'The median number is: {median}')
 
-            else:
-                median_index = len(nums) // 2
-                median = (nums[median_index] + nums[median_index - 1]) / 2
-                print(f'The median number is: {median}')
+        else:
+            median_index = len(nums) // 2
+            median = (nums[median_index] + nums[median_index - 1]) / 2
+            print(f'The median number is: {median}')
 
     return
 
@@ -63,17 +68,13 @@ def median():
 median()
 
 #mode
-from statistics import mode
 
 
 def data():
     try:
-        final_data_set = []
+
         # data_set = input('Please enter your data set. >  ')
-        data_set = data_set.split(',')
-        for i in range(len(data_set)):
-            i = data_set[i].strip(' ')
-            final_data_set.append(i)
+
         print(f"The MODE of this data set is: {mode(final_data_set)}")
         return
     except ValueError:
