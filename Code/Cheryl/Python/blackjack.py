a01888618e01f7c502192f5d2bd2025ccbe4a907
Cheryl Lewman <cheryllewman@gmail.com>
2018-09-26 14:19:41 -0700

    redoing the blackjack lab to make a game .

---
+++ Code/Cheryl/Python/blackjack.py
@@ -0,0 +1,9 @@
+welcome_mess = print('Welcome to Blackjack! You can practice and the computer will advise you on what to do, or you can play the game.')
+
+answer = print('Would you like to \'play\', \'practice\', or \'quit\'?')
+
+if answer == 'quit':
+    print('Thanks for playing!')
+
+elif answer == 'practice':
+    
