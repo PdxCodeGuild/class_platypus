da877c4814c40c59bf728543aa219ffec08a033f
Aceome <angelina.bowmer@gmail.com>
2018-10-01 15:13:42 -0700

    Django-polls

--- Code/Angie/Django/mysite/polls/views.py
+++ Code/Angie/Django/mysite/polls/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'polls/index.html')
+    return HttpResponse("Hello, world. You're at the polls index.")
+    # return render(request, 'polls/index.html')
+
+    # game_record = GameRecord(winner='nobody')
+    # game_record.save()
+    #
+    # return HttpResponse('game record successfully saved')
