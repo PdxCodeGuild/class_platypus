d0dfa4b6cc1cae8b496c428961e902ec38c5abaa
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-02 13:43:36 -0700

    working on lab 2.

--- Code/Cheryl/Django/mysite/todo/views.py
+++ Code/Cheryl/Django/mysite/todo/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def tst_vw(request):
+def index(request):
+    return render(request, 'todo/index.html', {})
+
+def add(request):
+    # receive the post request from the form in the template
+    if request.POST['add_item'] == True:
+        print('if working')
+    else:
+        print('else workign')
+    print(request.POST['add_item'])
     return HttpResponse('ok')
