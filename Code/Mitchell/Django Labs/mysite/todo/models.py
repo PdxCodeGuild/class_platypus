70f580479c86e53446d53fbe9b0763c9c35053ec
Mitchell Estes <mestes1530@gmail.com>
2018-10-02 15:26:58 -0700

    Added buttons to list items.

--- Code/Mitchell/Django Labs/mysite/todo/models.py
+++ Code/Mitchell/Django Labs/mysite/todo/models.py
@@ -3,9 +3,13 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
+
 class Item(models.Model):
     text = models.CharField(max_length=200)
     add_date = models.DateTimeField('date added', auto_now_add=True)
 
+
     def __str__(self):
         return self.text
+
--- Code/Mitchell/Django Labs/mysite/todo/views.py
+++ Code/Mitchell/Django Labs/mysite/todo/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tkinter import *
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the todo index.")
+    #btn = Button(top, text="Click Me")
+    # return HttpResponse("Hello, world. You're at the todo index.")
+    return render(request, 'todo/index.html', {'todos': ['a', 'b', 'c']})
