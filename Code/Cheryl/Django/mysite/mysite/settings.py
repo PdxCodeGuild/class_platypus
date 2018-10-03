82ef86a7290af18690b7a5a5e1094570eda8331d
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-03 15:35:15 -0700

    created basic structure magic 8 ball

--- Code/Cheryl/Django/mysite/mysite/settings.py
+++ Code/Cheryl/Django/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'magicball.apps.MagicballConfig',
     'todo.apps.TodoConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
