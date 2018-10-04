53a2851f20a26e0dd9686c5f71f4d9045bb1402f
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-04 10:01:17 -0700

    Finished the MagicBall app.

--- Code/Cheryl/Django/mysite/magicball/views.py
+++ Code/Cheryl/Django/mysite/magicball/views.py
@@ -7,17 +7,13 @@ from .models import OracleRecord
 def index(request):
 
     magicball = ['The future is cloudy.', 'It has been decided in your favor.', 'You never know.', 'Try Again', 'The outlook is favorable.']
-    choice = 0
-    while request.method == 'POST':
+    if request.method == 'POST':
         computer_choice = random.choice(magicball)
         oracle_record = OracleRecord(computer_choice=computer_choice)
         oracle_record.save()
-        if choice > 1:
-            return render(request, 'magicball/index.html', {'magicball': magicball, 'computer_choice': computer_choice})
-        else:
-            choice += 1
+        return render(request, 'magicball/index.html', {'computer_choice': computer_choice})
 
-    return render(request, 'magicball/index.html', {'magicball': magicball})
+    return render(request, 'magicball/index.html', {})
 
     #
     # if request.method == 'POST':
