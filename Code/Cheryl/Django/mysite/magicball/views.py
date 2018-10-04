a4ae8d4c29d1f6f492838b345b85f9a0be38a11e
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-03 20:32:14 -0700

    finished everything except I need help having it not show on first load.

--- Code/Cheryl/Django/mysite/magicball/views.py
+++ Code/Cheryl/Django/mysite/magicball/views.py
@@ -7,13 +7,24 @@ from .models import OracleRecord
 def index(request):
 
     magicball = ['The future is cloudy.', 'It has been decided in your favor.', 'You never know.', 'Try Again', 'The outlook is favorable.']
-
-    if request.method == 'POST':
+    choice = 0
+    while request.method == 'POST':
         computer_choice = random.choice(magicball)
         oracle_record = OracleRecord(computer_choice=computer_choice)
         oracle_record.save()
-        return render(request, 'magicball/index.html', {'magicball': magicball, 'computer_choice': computer_choice})
+        if choice > 1:
+            return render(request, 'magicball/index.html', {'magicball': magicball, 'computer_choice': computer_choice})
+        else:
+            choice += 1
 
     return render(request, 'magicball/index.html', {'magicball': magicball})
 
-
+    #
+    # if request.method == 'POST':
+    #     computer_choice = random.choice(magicball)
+    #     oracle_record = OracleRecord(computer_choice=computer_choice)
+    #     oracle_record.save()
+    #     return render(request, 'magicball/index.html', {'magicball': magicball, 'computer_choice': computer_choice})
+    #
+    # return render(request, 'magicball/index.html', {'magicball': magicball})
+    #
