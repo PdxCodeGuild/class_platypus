0c369497de11307504ec1333fcf70c4b2d767e57
Mitchell Estes <mestes1530@gmail.com>
2018-10-05 16:12:28 -0700

    Started working on the css and organization.

--- Code/Mitchell/Django Labs/sideProjects/socoolMediaManager/views.py
+++ Code/Mitchell/Django Labs/sideProjects/socoolMediaManager/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Platform
 
-
 def index(request):
     #return HttpResponse("Hello, world. You're at the Socool Media Manager's index.")
     platforms = Platform.objects.all()
