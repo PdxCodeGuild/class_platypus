50622cfe48748063bca1c19519d7ba3c4081333b
ColtonJP <colton.peterson06@gmail.com>
2018-10-05 10:55:28 -0700

    working on django todo list

--- Code/Colton/django/mysite/todo/models.py
+++ Code/Colton/django/mysite/todo/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Add(models.Model):
     thing = models.CharField(max_length=200)
     add_date = models.DateTimeField(default=datetime.datetime.now)
+    is_done = models.BooleanField(default=False)
 
     def __str__(self):
         return self.thing
