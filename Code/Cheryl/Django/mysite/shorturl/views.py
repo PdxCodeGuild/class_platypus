3eb014acd4f11fb95a23c59aee9b2abf594f5b22
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-06 07:43:21 -0700

    working on some old python labs and reorganizing.

--- Code/Cheryl/Django/mysite/shorturl/views.py
+++ Code/Cheryl/Django/mysite/shorturl/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
-
-
+import random
 from .models import Web_Url
+import string
 
 def index(request):
     return render(request, 'shorturl/index.html', {})
@@ -11,7 +11,14 @@ def index(request):
 def add(request):
     user_url = request.POST['Web_Url']
     # generate the short code, save it with the model
-    user_item = Web_Url(original_url=user_url)
-    user_item.save()
+    if request.method == 'POST':
+        for i in range(5):
+            code = random.choice(string.ascii_letters + string.digits)
+            i += 1
+            print(code)
+    code = Web_Url(original_url=user_url)
+    code.save()
     return HttpResponseRedirect(reverse('shorturl:index'))
+
+
 
