ba4584375180e9b46d5c1f1ec43c8a52f76b3f94
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-09 19:17:57 -0700

    added basic models and tested them in admin.

--- Code/Cheryl/Django/mysite/bibliothek/models.py
+++ Code/Cheryl/Django/mysite/bibliothek/models.py
@@ -1,12 +1,35 @@
-from django.db import models
 
-# Create your models here.
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Books(models.Model):
-    book_name = models.CharField(max_length=100)
-
+    title = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.book_name
+        return self.title
+
+
+class Circulation(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Author(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- Code/Cheryl/Django/mysite/bibliothek/views.py
+++ Code/Cheryl/Django/mysite/bibliothek/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 import random
-from .models import Books
+from .models import Books, Circulation, Author
 
 def index(request):
     return render(request, 'bibliothek/index.html', {})
