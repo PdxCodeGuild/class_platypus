bb4b2bf222975ea2cd77ca7af7c87e1c30862a3b
Mitchell Estes <mestes1530@gmail.com>
2018-10-10 10:56:21 -0700

    Saves long urls with random 6 character code.

--- Code/Mitchell/Django Labs/mysite/urlshort/urls.py
+++ Code/Mitchell/Django Labs/mysite/urlshort/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
-urlshort = 'urlshort'
+app_name = 'urlshort'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('saveUrl/', views.saveUrl, name='saveUrl'),
+    path('clearUrls/', views.clearUrls, name='clearUrls'),
 ]
--- Code/Mitchell/Django Labs/mysite/urlshort/views.py
+++ Code/Mitchell/Django Labs/mysite/urlshort/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Url
+import random, string
 
 def index(request):
-    return render(request, 'urlshort/index.html', {})
+    urls = Url.objects.all()
+    return render(request, 'urlshort/index.html', {'urls': urls})
+
+def saveUrl(request):
+    url_input = request.POST['url_input']
+    code = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+    url = Url(long_url=url_input, short_url=code)
+    url.save()
+    return HttpResponseRedirect(reverse('urlshort:index'))
+
+def redirect(request):
+    return HttpResponseRedirect(reverse('render/url:long_url/'))
+
+def clearUrls(request):
+    urls = Url.objects.all()
+    for url in urls:
+        url.delete()
+    return HttpResponseRedirect(reverse('urlshort:index'))
