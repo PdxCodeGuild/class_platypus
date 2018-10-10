86b385a8bf5c4eacd1c8e2dff652059849d581d8
Mitchell Estes <mestes1530@gmail.com>
2018-10-10 11:14:29 -0700

    Saved url codes link to their long url.

--- Code/Mitchell/Django Labs/mysite/urlshort/urls.py
+++ Code/Mitchell/Django Labs/mysite/urlshort/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('saveUrl/', views.saveUrl, name='saveUrl'),
     path('clearUrls/', views.clearUrls, name='clearUrls'),
+    path('redirect/<string:url_short>/', views.redirect, name='redirect'),
 ]
