8512810ad85893a14814ad45132a2053eb9d7920
Mitchell Estes <mestes1530@gmail.com>
2018-10-11 11:03:32 -0700

    Has the user specify the type of social media they are adding.

--- Code/Mitchell/Django Labs/sideProjects/socoolMediaManager/views.py
+++ Code/Mitchell/Django Labs/sideProjects/socoolMediaManager/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
-from .models import Platform
+from .models import Platform, PlatformType
 
 def index(request):
     platforms = Platform.objects.all()
-    return render(request, 'socoolMediaManager/index.html', {'platforms': platforms})
+    platform_types = PlatformType.objects.all()
+    return render(request, 'socoolMediaManager/index.html', {'platforms': platforms, 'platform_types': platform_types})
 
 def edit(request):
     platforms = Platform.objects.all()
-    return render(request, 'socoolMediaManager/edit.html', {'platforms': platforms})
+    platform_types = PlatformType.objects.all()
+    return render(request, 'socoolMediaManager/edit.html', {'platforms': platforms, 'platform_types': platform_types})
 
 def addPlatform(request):
     username_input = request.POST['username_input']
     link_input = request.POST['link_input']
-    platform = Platform(username=username_input, link=link_input)
+    platform_type_id = request.POST['platform_type_id']
+    platform = Platform(username=username_input, link=link_input, platform_type_id=platform_type_id)
     platform.save()
-    return HttpResponseRedirect(reverse('socoolMediaManager:index'))
+    return HttpResponseRedirect(reverse('socoolMediaManager:edit'))
 
 def deletePlatform(request):
     id = request.POST['id']
     platform = Platform.objects.get(pk=id)
     platform.delete()
-    return HttpResponseRedirect(reverse('socoolMediaManager:index'))
+    return HttpResponseRedirect(reverse('socoolMediaManager:edit'))
