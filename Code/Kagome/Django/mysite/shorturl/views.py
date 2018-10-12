b35e7291f9a885b53cbe50b5d3b5babe01901725
kagomesakura <kagomemiller@gmail.com>
2018-10-12 15:05:20 -0700

    url wip

--- Code/Kagome/Django/mysite/shorturl/views.py
+++ Code/Kagome/Django/mysite/shorturl/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+import random
+from .models import Short_Url
+import string
+
 
 def index(request):
-    return HttpResponse("URL Shortener!")
+    urls = Short_Url.objects.order_by('-id')
+    return render(request, 'shorturl/index.html', {'urls': urls})
 
-# Create your views here.
+
+def add(request):
+    user_url = request.POST['Short_Url']
+    random_chars = ''
+    for i in range(4):
+        random_chars += random.choice(string.ascii_letters + string.digits)
+    print(random_chars)
+    code = Short_Url(code=random_chars, original_url=user_url)
+    code.save()
+    return HttpResponseRedirect(reverse('shorturl:index'))
+
+
+def redirect(request, code):
+    url_row = Short_Url.objects.get(code=code)
+    return HttpResponseRedirect(url_row.original_url)
+
 
