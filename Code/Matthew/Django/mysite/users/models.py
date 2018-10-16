9f574285c5afcfcc7020b5f21f6d7f221994e68e
flux2341 <flux2341@gmail.com>
2018-10-16 10:13:48 -0700

    added library app

--- Code/Matthew/Django/mysite/users/models.py
+++ Code/Matthew/Django/mysite/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# class BookCheckout(models.Model):
+#     book = models.ForeignKey(Book)
+#     user = models.ForeignKey(User)
