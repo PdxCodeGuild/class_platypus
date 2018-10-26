ece70c46ad295a0c5fef8931a3441cf1a4383705
Cheryl Lewman <cheryllewman@gmail.com>
2018-10-26 07:09:28 -0700

    working through data.

--- Code/Cheryl/Django/demograph/demograph/data.py
+++ Code/Cheryl/Django/demograph/demograph/data.py
@@ -4,10 +4,12 @@ import requests
 import json
 
 #api key/token
+api_key = '35cfed388d369e78f29d2be64e38f5365b7c92c9'
+api_url_base = 'https://api.census.gov/data/2014/pep/cochar6/variables.json'
 
-api_url_base = 'https://api.census.gov/data.json'
 
 def get_json(url):
+    print('loading ' + url + '...')
     return json.loads(requests.get(url).text)
 
 def get_data(title, datasets):
@@ -28,8 +30,19 @@ def get_data(title, datasets):
             print(values)
 
 
+def print_dataset_names(datasets):
+    names = []
+    for dataset in datasets['dataset']:
+        names.append(dataset['title'])
+    names.sort()
+    print('\n'.join(names))
+
 datasets = get_json(api_url_base)
-get_data('County Total Population and Components of Change', datasets)
+# print(datasets)
+# print_dataset_names(datasets)
+dataset_name = 'Vintage 2017 Population Estimates: Population Estimates'
+# dataset_name = 'County Total Population and Components of Change'
+get_data(dataset_name, datasets)
 
 
 
