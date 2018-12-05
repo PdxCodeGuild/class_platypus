382df66d6f8397d81b46185e3fe11055a1b8782d
Aceome <angelina.bowmer@gmail.com>
2018-12-04 19:25:46 -0800

    updating portfolio

--- Code/Angie/Django/mysite/library/admin.py
+++ Code/Angie/Django/mysite/library/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Author
 from .models import Book
-# from .models import CheckSystem
+
 
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Book)
-# admin.site.register(CheckSystem)
